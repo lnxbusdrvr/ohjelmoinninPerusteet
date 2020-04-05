6214ec2daac2b20371212ec59f6a7d0f380abb54
Erkki Pokkinen <anticop@iki.fi>
2020-04-05 14:10:20 +0300

    Create PoistaViimeinen.java

---
+++ moocJava2020/Osa03_17.PoistaViimeinen/PoistaViimeinen.java
@@ -0,0 +1,3 @@
+Luo tehtäväpohjaan metodi public static void poistaViimeinen(ArrayList<String> mjonot).
+Metodin tulee poistaa parametrina saadusta listasta viimeisin arvo.
+Mikäli lista on tyhjä, metodin ei tule tehdä mitään.
