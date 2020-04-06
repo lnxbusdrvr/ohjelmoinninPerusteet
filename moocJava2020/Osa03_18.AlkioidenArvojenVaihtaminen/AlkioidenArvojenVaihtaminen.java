b12e4e9d9950f4f270915fcf503e19c753a2af31
Erkki Pokkinen <anticop@iki.fi>
2020-04-06 16:49:01 +0300

    Koodauksen miettimisen aloitus

---
+++ moocJava2020/Osa03_18.AlkioidenArvojenVaihtaminen/AlkioidenArvojenVaihtaminen.java
@@ -0,0 +1,23 @@
+Ohjelmointitehtävä:
+Alkioiden arvojen vaihtaminen
+Pisteitä
+0/1
+
+Tehtäväpohjaan on toteutettu valmiiksi ohjelma, missä luodaan taulukko sekä tulostetaan taulukon arvot kahteen kertaan. Muokkaa ohjelmaa siten, että sen jälkeen kun taulukon arvot on tulostettu ensimmäiseen kertaan, käyttäjältä kysytään kahta indeksiä, joiden osoittamat arvot vaihdetaan taulukossa päittäin. Tämän jälkeen alkiot tulee vaihtaa päittäin ja taulukon arvot tulostaa toiseen kertaan.
+Esimerkkitulostus
+
+1
+3
+5
+7
+9
+
+Mitkä indeksit vaihdetaan?
+2
+4
+
+1
+3
+9
+7
+5
