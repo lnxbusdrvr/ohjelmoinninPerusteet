5d413433580947407ecc31c0caa79aecb029c668
Erkki Pokkinen <anticop@iki.fi>
2020-04-06 17:11:05 +0300

    Koodiaika 6min 46sek

--- moocJava2020/Osa03_18.AlkioidenArvojenVaihtaminen/AlkioidenArvojenVaihtaminen.java
+++ moocJava2020/Osa03_18.AlkioidenArvojenVaihtaminen/AlkioidenArvojenVaihtaminen.java
@@ -1,23 +1,39 @@
-Ohjelmointitehtävä:
-Alkioiden arvojen vaihtaminen
-Pisteitä
-0/1
 
-Tehtäväpohjaan on toteutettu valmiiksi ohjelma, missä luodaan taulukko sekä tulostetaan taulukon arvot kahteen kertaan. Muokkaa ohjelmaa siten, että sen jälkeen kun taulukon arvot on tulostettu ensimmäiseen kertaan, käyttäjältä kysytään kahta indeksiä, joiden osoittamat arvot vaihdetaan taulukossa päittäin. Tämän jälkeen alkiot tulee vaihtaa päittäin ja taulukon arvot tulostaa toiseen kertaan.
-Esimerkkitulostus
+import java.util.Scanner;
 
-1
-3
-5
-7
-9
+public class AlkioidenArvojenVaihtaminen {
 
-Mitkä indeksit vaihdetaan?
-2
-4
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        int[] taulukko = new int[5];
+        taulukko[0] = 1;
+        taulukko[1] = 3;
+        taulukko[2] = 5;
+        taulukko[3] = 7;
+        taulukko[4] = 9;
 
-1
-3
-9
-7
-5
+        int indeksi = 0;
+        while (indeksi < taulukko.length) {
+            System.out.println(taulukko[indeksi]);
+            indeksi++;
+        }
+        System.out.println("");
+
+        // Toteuta indeksien kysyminen ja vaihto tänne
+        System.out.println("Mitkä indeksit vaihdetaan?");
+        int eka = Integer.valueOf(lukija.nextLine());
+        int toka = Integer.valueOf(lukija.nextLine());
+        
+        int apulainen = taulukko[eka];
+        taulukko[eka] = taulukko[toka];
+        taulukko[toka] = apulainen;
+
+        System.out.println("");
+        indeksi = 0;
+        while (indeksi < taulukko.length) {
+            System.out.println(taulukko[indeksi]);
+            indeksi++;
+        }
+    }
+
+}
