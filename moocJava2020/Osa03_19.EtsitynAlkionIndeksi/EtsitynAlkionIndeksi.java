8621d108c7f8ae874f39fa495fb90a75ef26b469
Erkki Pokkinen <anticop@iki.fi>
2020-04-08 12:21:21 +0300

    Koodausaika 10min 18sek

--- moocJava2020/Osa03_19.EtsitynAlkionIndeksi/EtsitynAlkionIndeksi.java
+++ moocJava2020/Osa03_19.EtsitynAlkionIndeksi/EtsitynAlkionIndeksi.java
@@ -1,9 +1,37 @@
-Tehtäväpohjassa on valmiina taulukko, joka sisältää lukuja. Täydennä ohjelmaa siten, että käyttäjältä kysyttyä lukua etsitään taulukosta. Jos luku löytyy taulukosta, ohjelma kertoo luvun indeksin. Jos lukua taas ei löydy taulukosta, ohjelma kertoo ettei lukua löydy.
-Esimerkkitulostus
 
-Mitä etsitään? 3
-Luku 3 löytyy indeksistä 4.
-Esimerkkitulostus
 
-Mitä etsitään? 7
-Luku 7 löytyy indeksistä 7.
+import java.util.Scanner;
+
+public class EtsitynAlkionIndeksi {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        int[] taulukko = new int[10];
+        taulukko[0] = 6;
+        taulukko[1] = 2;
+        taulukko[2] = 8;
+        taulukko[3] = 1;
+        taulukko[4] = 3;
+        taulukko[5] = 0;
+        taulukko[6] = 9;
+        taulukko[7] = 7;
+
+        System.out.print("Mitä etsitään? ");
+        int etsittava = Integer.valueOf(lukija.nextLine());
+
+        // Toteuta etsimistoiminnallisuus tänne
+        int i = 0;
+        
+        while(i < taulukko.length) {
+            System.out.println(taulukko[i]);
+            int sisalto = taulukko[i];
+            if(sisalto == etsittava) {
+                System.out.println("Luku " +etsittava+ " löytyy indeksistä "+i+".");
+            }
+            System.out.println("Lukua "+etsittava+" ei löydy.");
+            
+            i++;
+        }
+    }
+
+}
