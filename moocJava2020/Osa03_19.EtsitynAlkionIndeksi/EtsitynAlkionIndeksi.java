cda042e3e9b101f830b027fa2bca52b665f789d4
Erkki Pokkinen <anticop@iki.fi>
2020-04-08 11:46:57 +0300

    tehtävä

---
+++ moocJava2020/Osa03_19.EtsitynAlkionIndeksi/EtsitynAlkionIndeksi.java
@@ -0,0 +1,9 @@
+Tehtäväpohjassa on valmiina taulukko, joka sisältää lukuja. Täydennä ohjelmaa siten, että käyttäjältä kysyttyä lukua etsitään taulukosta. Jos luku löytyy taulukosta, ohjelma kertoo luvun indeksin. Jos lukua taas ei löydy taulukosta, ohjelma kertoo ettei lukua löydy.
+Esimerkkitulostus
+
+Mitä etsitään? 3
+Luku 3 löytyy indeksistä 4.
+Esimerkkitulostus
+
+Mitä etsitään? 7
+Luku 7 löytyy indeksistä 7.
