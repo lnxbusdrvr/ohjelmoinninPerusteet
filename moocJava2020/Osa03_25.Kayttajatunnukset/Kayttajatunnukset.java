ce4cc13e5421e210941d994f46e3f26dd869ff61
Erkki Pokkinen <anticop@iki.fi>
2020-04-10 16:41:16 +0300

    Koodausaika 13min 52sek

--- moocJava2020/Osa03_25.Kayttajatunnukset/Kayttajatunnukset.java
+++ moocJava2020/Osa03_25.Kayttajatunnukset/Kayttajatunnukset.java
@@ -22,4 +22,33 @@
  * Anna salasana: jokerit
  * Virheellinen tunnus tai salasana!
  */
- 
+
+import java.util.Scanner;
+
+public class Kayttajatunnukset {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        System.out.print("Anna tunnus: ");
+        String tunnus = lukija.nextLine();
+        System.out.print("Anna salasana: ");
+        String salasana = lukija.nextLine();
+        
+        /*
+         * aleksi	tappara
+         * elina	kissa
+         */
+        String tunnusYks = "aleksi";
+        String tunnusKaks = "elina";
+        String salasanaYks = "tappara";
+        String salasanaKaks = "kissa";
+        
+        if(tunnus.equals(tunnusYks) && salasana.equals(salasanaYks) 
+        || tunnus.equals(tunnusKaks) && salasana.equals(salasanaKaks)) {
+            System.out.println("Olet kirjautunut järjestelmään");
+        }else{
+            System.out.println("Virheellinen tunnus tai salasana!");
+        }
+    }
+} 
