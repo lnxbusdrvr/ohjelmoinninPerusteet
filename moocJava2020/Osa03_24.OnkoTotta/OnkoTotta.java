91f9373ffed30714fe12857dfb9fc2be7a01e9e0
Erkki Pokkinen <anticop@iki.fi>
2020-04-10 16:07:52 +0300

    Koodausaika 2min 27sek

--- moocJava2020/Osa03_24.OnkoTotta/OnkoTotta.java
+++ moocJava2020/Osa03_24.OnkoTotta/OnkoTotta.java
@@ -8,4 +8,23 @@
  * Kirjoita merkkijono: tottapa
  * Koitappa uudelleen!
  */
+
+import java.util.Scanner;
+
+public class OnkoTotta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        System.out.print("Kirjoita merkkijono: ");
+        String kysymys = lukija.nextLine();
+        
+        if(kysymys.equals("totta")) {
+            System.out.println("Oikein meni!");
+        }else{
+            System.out.println("Koitappa uudelleen!");
+        }
+
+    }
+}
  
