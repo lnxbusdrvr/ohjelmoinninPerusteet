8fddfd6c61ca11ed26f8a83fcfd4d6db078af598
Erkki Pokkinen <anticop@iki.fi>
2020-04-10 16:03:45 +0300

    Tehtävä

---
+++ moocJava2020/Osa03_24.OnkoTotta/OnkoTotta.java
@@ -0,0 +1,11 @@
+/* Tee ohjelma, joka pyytää käyttäjää kirjoittamaan merkkijonon. Jos käyttäjä kirjoittaa merkkijonon "totta", tulostetaan merkkijono "Oikein meni!", muulloin tulostetaan merkkijono "Koitappa uudelleen!".
+ * Esimerkkitulostus
+ *
+ * Kirjoita merkkijono: totta
+ * Oikein meni!
+ * Esimerkkitulostus
+ *
+ * Kirjoita merkkijono: tottapa
+ * Koitappa uudelleen!
+ */
+ 
