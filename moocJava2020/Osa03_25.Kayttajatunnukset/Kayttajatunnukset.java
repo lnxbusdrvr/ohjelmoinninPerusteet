3a87c2c948ed324ae9bdb111a88b0602688df440
Erkki Pokkinen <anticop@iki.fi>
2020-04-10 16:16:07 +0300

    Tehtävä

---
+++ moocJava2020/Osa03_25.Kayttajatunnukset/Kayttajatunnukset.java
@@ -0,0 +1,25 @@
+/*
+ * Tee ohjelma, joka tunnistaa seuraavat käyttäjät:
+ * tunnus	salasana
+ * aleksi	tappara
+ * elina	kissa
+ *
+ * Ohjelma näyttää käyttäjälle henkilökohtaisen viestin tai ilmoittaa, jos tunnus tai salasana on väärin.
+ *
+ * Esimerkkitulostus:
+ *
+ * Anna tunnus: aleksi
+ * Anna salasana: tappara
+ * Olet kirjautunut järjestelmään
+ * Esimerkkitulostus
+ 
+ * Anna tunnus: elina
+ * Anna salasana: kissa
+ * Olet kirjautunut järjestelmään
+ * Esimerkkitulostus
+ *
+ * Anna tunnus: aleksi
+ * Anna salasana: jokerit
+ * Virheellinen tunnus tai salasana!
+ */
+ 
