c0ce201ca5b79e945b1e66a7f70c2e1395ea514d
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 13:42:17 +0300

    Tehtävä

---
+++ moocJava2020/osa03/Osa03_26.SanatRiveittain/SanatRiveittain.java
@@ -0,0 +1,21 @@
+/*
+ * Kirjoita ohjelma, joka lukee käyttäjältä merkkijonoja.
+ * Mikäli syötetty merkkijono on tyhjä, ohjelma lopettaa käyttäjältä lukemisen
+ * ja ohjelman suoritus päättyy.
+ * Mikäli merkkijono ei ole tyhjä,
+ * ohjelma pilkkoo syötetyn merkkijonon osiksi välilyöntien kohdalta
+ * ja tulostaa pilkotun merkkijonon osat omille riveilleen.
+ *
+ * Esimerkkitulostus:
+ *
+ * olipa kerran
+ * olipa
+ * kerran
+ * pieni ohjelma joka
+ * pieni
+ * ohjelma
+ * joka
+ * loppui
+ * loppui
+ */
+ 
