feb3be00334566c0489bb37447d0d00b4b92d518
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 18:45:33 +0300

    tehtävä

---
+++ moocJava2020/osa03/Osa03_29.ViimeisetSanat/ViimeisetSanat.java
@@ -0,0 +1,17 @@
+/*
+ * Kirjoita ohjelma, joka lukee käyttäjältä merkkijonoja.
+ * Mikäli syötetty merkkijono on tyhjä, ohjelma ei jatka lukemista
+ * ja ohjelman suoritus päättyy. Mikäli merkkijono ei ole tyhjä,
+ * ohjelma pilkkoo syötetyn merkkijonon osiksi välilyöntien kohdalta
+ * ja tulostaa kunkin pilkotun merkkijonon viimeisen osan.
+ *
+ * Esimerkkitulostus:
+ * 
+ * yksi kaksi kolme neljä
+ * neljä
+ * viestin purku tässä selvä
+ * selvä
+ *
+ */
+ 
+ 
