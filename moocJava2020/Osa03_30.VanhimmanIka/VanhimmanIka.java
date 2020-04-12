c19f9e36d002b99c41affb3d81298a64247d0c2f
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 21:38:06 +0300

    Koodausaika 51min
    Miljoonaa tuntiä meni siihen, että unohti, kun nro 2 ei tarkoita toista indexin alkoita, siksi pitkä koodiaika, huoh.
    #KaksiEiOleKaksi #YksiOnKaksi

--- moocJava2020/Osa03_30.VanhimmanIka/VanhimmanIka.java
+++ moocJava2020/Osa03_30.VanhimmanIka/VanhimmanIka.java
@@ -20,3 +20,42 @@
  *
  */
  
+
+import java.util.Scanner;
+
+public class VanhimmanIka {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        int suurin = 0;
+                
+        while(true) {
+            String syote = lukija.nextLine();
+            if(syote.equals("")) {
+                break;
+            }
+            String[] palat = syote.split(",");
+            
+            // Miljoonaa tuntiä meni siihen,
+            // että unohti, kun nro 2 ei tarkoita
+            // toista indexin alkoita
+            for(String pala : palat) {
+                int palaYks = Integer.parseInt(palat[1]); // eli indexi kaksi
+                if(palaYks > suurin) {
+                    suurin = palaYks;
+                }
+            }
+            
+            /*
+            for(int i = 0; i < palat.length; i++) {
+                int pala1 = Integer.parseInt(palat[2]); // Facepalm, koska 2 ei ole 2
+                if(pala1 > suurin) {
+                    suurin = pala1;
+                }
+            }*/
+            
+        }
+        System.out.println("Vanhimman ikä: " +suurin);
+    }
+}
