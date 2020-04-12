49bdb6fa81d6c36b1a9d6bdf9dcb3475bf488be3
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 18:53:40 +0300

    Koodausaika 4min

--- moocJava2020/osa03/Osa03_29.ViimeisetSanat/ViimeisetSanat.java
+++ moocJava2020/osa03/Osa03_29.ViimeisetSanat/ViimeisetSanat.java
@@ -15,3 +15,22 @@
  */
  
  
+import java.util.Scanner;
+
+public class ViimeisetSanat {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        String syote = lukija.nextLine();
+        
+        while(!syote.isEmpty()) {
+            String[] paloita = syote.split(" ");
+            
+            int viimeinen = paloita.length - 1;
+            System.out.println(paloita[viimeinen]);
+            
+            syote = lukija.nextLine();
+        }
+    }
+}
