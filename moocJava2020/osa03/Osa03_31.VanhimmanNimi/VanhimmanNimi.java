60aeced4ca80ef9145f3bf0ed2e810e0678607f5
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 22:05:14 +0300

    Koodausaika 4min

--- moocJava2020/osa03/Osa03_31.VanhimmanNimi/VanhimmanNimi.java
+++ moocJava2020/osa03/Osa03_31.VanhimmanNimi/VanhimmanNimi.java
@@ -18,3 +18,33 @@
  *
  */
  
+import java.util.Scanner;
+
+public class VanhimmanNimi {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        
+        // Lainaus edelisestä tehtävästä
+        int suurin = 0;
+        String vanhinNimi = null;
+                
+        while(true) {
+            String syote = lukija.nextLine();
+            if(syote.equals("")) {
+                break;
+            }
+            String[] palat = syote.split(",");
+            
+            for(String pala : palat) {
+                int palaYks = Integer.parseInt(palat[1]); // eli indexi kaksi
+                String nimi = palat[0];
+                if(palaYks > suurin) {
+                    suurin = palaYks;
+                    vanhinNimi = nimi;
+                }
+            }
+        }
+        System.out.println("Vanhimman ikä: " +vanhinNimi);
+    }
+}
