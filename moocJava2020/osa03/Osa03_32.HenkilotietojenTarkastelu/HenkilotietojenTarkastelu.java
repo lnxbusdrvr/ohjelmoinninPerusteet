c81e6d0c3b63e4eb8b02df013ed378e09a46b391
Erkki Pokkinen <anticop@iki.fi>
2020-04-12 22:16:08 +0300

    tehtävä

---
+++ moocJava2020/osa03/Osa03_32.HenkilotietojenTarkastelu/HenkilotietojenTarkastelu.java
@@ -0,0 +1,35 @@
+/*
+ * Kirjoita ohjelma, joka lukee käyttäjältä henkilötietoja.
+ * Henkilötiedot sisältävät etunimen ja syntymävuoden pilkulla eroteltuna.
+ * TIetojen lukemista jatketaan kunnes käyttäjä syöttää tyhjän merkkijonon.
+ * 
+ * Kun lukeminen lopetetaan,
+ * ohjelman tulee tulostaa pisin luettu etunimi
+ * sekä luettujen henkilöiden syntymävuosien keskiarvo.
+ * Mikäli pisimpiä etunimiä on useita, voit tulostaa niistä minkä tahansa.
+ * Voit olettaa, että käyttäjä syöttää aina vähintään yhden henkilötiedon.
+ *
+ * Esimerkkitulostus:
+ *
+ * leevi,2017
+ * anton,2017
+ * lilja,2017
+ * venla,2014
+ * gabriel,2009
+ *
+ * Pisin nimi: gabriel
+ * Syntymävuosien keskiarvo: 2014.8
+ *
+ * Esimerkkitulostus:
+ *
+ * sauli,1948
+ * tarja,1943
+ * martti,1936
+ * mauno,1923
+ * urho,1900
+ *
+ * Pisin nimi: martti
+ * Syntymävuosien keskiarvo: 1930.0
+ *
+ */
+ 
