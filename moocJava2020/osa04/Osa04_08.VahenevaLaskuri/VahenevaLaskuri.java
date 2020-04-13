7ee93fb2795ebbccb8a08a7368e4f825a13cebfe
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 22:05:32 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_08.VahenevaLaskuri/VahenevaLaskuri.java
@@ -0,0 +1,26 @@
+/*
+ * Osa1: Metodin vahene() toteutus
+ * -------------------------------
+ * Täydennä luokan runkoon metodin vahene() toteutus
+ * sellaiseksi, että se vähentää kutsuttavan olion oliomuuttujan
+ * arvo arvoa yhdellä.
+ * Kun olet toteuttanut metodin vahene(), 
+ * 
+ * Osa2: Laskurin arvo ei saa olla negatiivinen
+ * --------------------------------------------
+ * Täydennä metodin vahene() toteutus sellaiseksi,
+ * ettei laskurin arvo mene koskaan negatiiviseksi.
+ * Eli jos laskurin arvo on jo 0,
+ * ei vähennys sitä enää vähennä. Ehtolause auttaa tässä.
+ *
+ * Osa3: Laskurin arvon nollaus
+ * ----------------------------
+ * Tee laskurille metodi public void nollaa()
+ * joka nollaa laskurin arvon, esim:
+ *
+ * arvo: 100
+ * arvo: 0
+ * arvo: 0
+ *
+ */
+ 
