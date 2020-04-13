a35be8016274b00e51b4118fc57e6e83555905d1
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 14:16:13 +0300

    Ei minun kirjoittamaa koodia

---
+++ moocJava2020/osa04/Osa04_01.EnsimmainenTilisi/Tili.java
@@ -0,0 +1,36 @@
+/*
+ * Tämä luokka ei ole minun kirjoittama/koodama
+ * vaan mooc 2020 -koodia
+ */
+ 
+ /* 
+ * ÄLÄ KOSKE TÄÄLLÄ OLEVAAN KOODIIN!
+ */
+
+public class Tili {
+
+    private double saldo;
+    private String omistaja;
+
+    public Tili(String omistaja, double saldo) {
+        this.saldo = saldo;
+        this.omistaja = omistaja;
+    }
+
+    public void pano(double maara) {
+        this.saldo = this.saldo + maara;
+    }
+
+    public void otto(double maara) {
+        this.saldo = this.saldo - maara;
+    }
+
+    public double saldo() {
+        return this.saldo;
+    }
+
+    @Override
+    public String toString() {
+        return this.omistaja + " saldo: " + this.saldo;
+    }
+}
