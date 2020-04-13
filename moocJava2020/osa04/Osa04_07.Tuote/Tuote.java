da1dce62ab1a44dbab84353153f6bc91a54f9a34
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 21:40:10 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_07.Tuote/Tuote.java
@@ -0,0 +1,33 @@
+/*
+ * Luo luokka Tuote, joka esittää kaupan tuotetta.
+ * Tuotteella tulee olla 
+ * hinta (double)
+ * lukumäärä (int)
+ * nimi (String)
+ *
+ * Luokalla tulee olla:
+ *
+ * -Konstruktori
+ *
+ * public Tuote(String nimiAlussa, double hintaAlussa, int maaraAlussa)
+ *
+ * -Metodi
+ *
+ * public void tulostaTuote()
+ *
+ * joka tulostaa tuotteen tiedot tässä muodossa:
+ *
+ * Esimerkkitulostus:
+ *
+ * Banaani, hinta 1.1, 13 kpl
+ *
+ * Yllä oleva tulostus olettaa,
+ * että tuotteen nimeksi on annettu
+ * banaani,
+ * hinnaksi on annettu
+ * 1.1,
+ * ja määräksi on annettu
+ * 13.
+ *
+ */
+ 
