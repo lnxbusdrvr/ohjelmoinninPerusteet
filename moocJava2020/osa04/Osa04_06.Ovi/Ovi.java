f8bee84eafc03ad971c615fbc65edc0e8918a383
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 21:26:48 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_06.Ovi/Ovi.java
@@ -0,0 +1,25 @@
+/*
+ * Luo luokka nimeltä Ovi.
+ * Ovella ei ole oliomuuttujia.
+ * Luo sille parametriton konstruktori (tai käytä oletuskonstruktoria).
+ * Luo tämän jälkeen ovelle metodi
+ *
+ * public void koputa()
+ *
+ * jota kutsuttaessa tulostuu viesti
+ * "Who's there?".
+ *
+ * Oven tulee toimia seuraavasti.
+ *
+ * Ovi alexander = new Ovi();
+ *
+ * alexander.koputa();
+ * alexander.koputa();
+ *
+ * Esimerkkitulostus:
+ *
+ * Who's there?
+ * Who's there?
+ *
+ */
+ 
