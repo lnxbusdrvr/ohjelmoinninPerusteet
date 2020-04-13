93b345bb8de2807c61cde4d570fe4e0ba91f6b41
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 14:26:12 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_02.EnsimmainenTilisiirtosi/EnsimmainenTilisiirtosi.java
@@ -0,0 +1,13 @@
+/*
+ * Tässäkin tehtävässä on käytössä edellisessä tehtävässä mukana ollut luokka Tili.
+ *
+ * Tee ohjelma joka:
+ *
+ *   Luo tilin nimeltä "Matin tili" saldolla 1000
+ *   Luo tilin nimeltä "Oma tili" saldolla 0
+ *   Nostaa matin tililtä 100.0
+ *   Panee omalle tilille 100.0
+ *   Tulostaa molemmat tilit
+ *
+ */
+ 
