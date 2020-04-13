dc467b45b8cb10af572f2d841d98c05425dc81d2
Erkki Pokkinen <anticop@iki.fi>
2020-04-13 20:49:42 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_04.Huone/Huone.java
@@ -0,0 +1,23 @@
+/*
+ * Luo luokka nimeltä Huone.
+ * Lisää luokalle oliomuuttujat
+ *
+ * private String koodi
+ * private int istumapaikat
+ *
+ * Luo tämän jälkeen konstruktori
+ * public Huone(String luokanKoodi, int istumapaikkojenMaara)
+ *
+ * jonka avulla oliomuuttujiin asetetaan arvot.
+ *
+ * Luokkakaavio:
+ *
+ * |        Huone        |
+ * |---------------------|
+ * | -koodi: String      |
+ * | -istumapaikat: int  |
+ * |---------------------|
+ * | +Huone(String, int) |
+ *
+ */
+ 
