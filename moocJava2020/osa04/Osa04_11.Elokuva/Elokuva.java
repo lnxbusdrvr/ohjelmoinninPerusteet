02e1417fb9f65f53f5f76e25d729dc4d0b291fd3
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-14 22:28:36 +0300

    Koodausaika 2min

--- moocJava2020/osa04/Osa04_11.Elokuva/Elokuva.java
+++ moocJava2020/osa04/Osa04_11.Elokuva/Elokuva.java
@@ -5,4 +5,21 @@
  * Ensimmäinen palauttaa elokuvan nimen ja toinen elokuvan ikärajan.
  *
  */
- 
+
+public class Elokuva {
+    private String nimi;
+    private int ikaraja;
+    
+    public Elokuva(String elokuvanNimi, int elokuvanIkaraja) {
+        this.nimi = elokuvanNimi;
+        this.ikaraja = elokuvanIkaraja;
+    }
+    
+    public String nimi() {
+        return nimi;
+    }
+    
+    public int ikaraja() {
+        return ikaraja;
+    }    
+}
