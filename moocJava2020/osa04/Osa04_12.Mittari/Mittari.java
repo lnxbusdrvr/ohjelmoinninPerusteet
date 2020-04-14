ee5280c4036dbcf98b6c6f9a7bce3e17ac7e749c
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-14 22:53:20 +0300

    Koodausaika 5min 25sek

--- moocJava2020/osa04/Osa04_12.Mittari/Mittari.java
+++ moocJava2020/osa04/Osa04_12.Mittari/Mittari.java
@@ -11,3 +11,37 @@
  */
  
  
+
+
+public class Mittari {
+    private int mitta;
+    
+    public Mittari() {
+        this.mitta = 0;
+    }
+    
+    public void lisaa() {
+        this.mitta = this.mitta + 1;
+        if(this.mitta > 5) {
+            this.mitta = this.mitta -1;
+        }
+    }
+    
+    public void vahenna() {
+        this.mitta = this.mitta -1;
+        if(this.mitta < 0) {
+            this.mitta = this.mitta +1;
+        }
+    }
+    
+    public int mitta() {
+        return this.mitta;
+    }
+    
+    public boolean taynna() {
+        if(this.mitta == 5) {
+            return true;
+        }
+        return false;
+    }
+}
