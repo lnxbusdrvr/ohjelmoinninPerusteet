d0e480547aa3a192da8c5224914a4f2838346e97
Erkki Pokkinen <anticop@iki.fi>
2020-04-14 16:16:15 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_09.Velka/Velka.java
@@ -0,0 +1,19 @@
+/*
+ * Luo luokka Velka, jolla on double-tyyppiset oliomuuttujat
+ * saldo ja korkokerroin.
+ *
+ * Saldo ja korkokerroin annetaan konstruktorin parametrina
+ *
+ * public Velka(double saldoAlussa, double korkokerroinAlussa).
+ *
+ * Luo luokalle myös metodit
+ *
+ * public void tulostaSaldo()
+ * public void odotaVuosi()
+ *
+ * Metodi tulostaSaldo tulostaa tämän hetkisen saldon,
+ * ja metodi odotaVuosi kasvattaa velan määrää.
+*
+* Velan määrän kasvattaminen tapahtuu kertomalla saldo korkokertoimella.
+*
+*/
