fa1e3578562237d3739a84902a9c22cc47c1fd40
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-14 22:23:08 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_11.Elokuva/Elokuva.java
@@ -0,0 +1,8 @@
+/*
+ * Luo luokka Elokuva, jolla on oliomuuttujat nimi (String) ja ikaraja (int).
+ * Tee luokalle konstruktori public Elokuva(String elokuvanNimi, int elokuvanIkaraja)
+ * sekä metodit public String nimi() ja public int ikaraja().
+ * Ensimmäinen palauttaa elokuvan nimen ja toinen elokuvan ikärajan.
+ *
+ */
+ 
