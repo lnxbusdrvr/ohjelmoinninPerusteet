a6e60e13610a574fa14ff495a41951db644ef010
Erkki Pokkinen <anticop@iki.fi>
2020-04-14 22:15:50 +0300

    tehtävä

---
+++ moocJava2020/osa04/Osa04_10.Musiikkikappale/Musiikkikappale.java
@@ -0,0 +1,29 @@
+/*
+ * Luo luokka nimeltä Musiikkikappale.
+ * Musiikkikappaleella on oliomuuttujat
+ *
+ * nimi (merkkijono)
+ * pituus sekunteina (kokonaisluku)
+ *
+ * Molemmat asetetaan konstruktorissa
+ *
+ * public Musiikkikappale(String kappaleenNimi, int kappaleenPituus)
+ *
+ * Lisää oliolle myös metodit
+ *
+ * public String nimi()
+ *
+ * joka palauttaa kappaleen nimen
+ *
+ * public int pituus()
+ *
+ * joka palauttaa kappaleen pituuden.
+ *
+ * Luokan tulee toimia seuraavasti.
+ *
+ * Musiikkikappale garden = new Musiikkikappale("In The Garden", 10910);
+ * System.out.println("Kappaleen " + garden.nimi() + " pituus on " + garden.pituus() + " sekuntia.");
+ *
+ */
+ 
+ 
