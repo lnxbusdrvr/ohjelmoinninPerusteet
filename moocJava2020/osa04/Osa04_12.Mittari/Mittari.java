8872097c44e0487288b7f49397a220521d54a5b2
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-14 22:41:42 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_12.Mittari/Mittari.java
@@ -0,0 +1,13 @@
+/*
+ * Luo luokka Mittari. Mittarilla on oliomuuttuja
+ * private int mitta, parametriton konstruktori (asettaa muuttujan mitta alkuarvoksi 0),
+ * sekä seuraavat neljä metodia:
+ *
+ *   Metodi public void lisaa() kasvattaa oliomuuttujan mitta arvoa yhdellä. Ei kasvata arvoa yli viiden.
+ *   Metodi public void vahenna() vähentää oliomuuttujan mitta arvoa yhdellä. Ei vähennä arvoa negatiiviseksi.
+ *   Metodi public int mitta() palauttaa oliomuuttujan mitta arvon.
+ *   Metodi public boolean taynna() palauttaa true mikäli oliomuuttujan mitta on viisi, palauttaa muulloin false.
+ *
+ */
+ 
+ 
