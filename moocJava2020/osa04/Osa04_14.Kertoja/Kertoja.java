9d2c1ea88672608e5153e39a543e048c38591cc9
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-15 20:13:31 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_14.Kertoja/Kertoja.java
@@ -0,0 +1,13 @@
+/*
+ * Luo luokka Kertoja jolla on:
+ *
+ *  Konstruktori public Kertoja(int luku).
+ *  Metodi public int kerro(int luku)
+ *
+ * joka palauttaa sille annetun luvun
+ * luku kerrottuna konstruktorille annetulla luvulla luku.
+ *
+ * Tarvinnet tässä myös oliomuuttujan...
+ *
+ */
+ 
