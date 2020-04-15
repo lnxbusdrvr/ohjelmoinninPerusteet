c8798e6d17bfbd533e1081dd4316397cdc076673
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-15 19:52:48 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_13.Agentti/Agentti.java
@@ -0,0 +1,21 @@
+/*
+ * Tehtäväpohjassa on määriteltynä luokka
+ * Agentti
+ * jolla on
+ * etunimi 
+ * sukunimi
+ *
+ * Luokalle on määritelty metodi tulosta,
+ *
+ * Esimerkkitulostus:
+ *
+ * My name is Bond, James Bond
+ *
+ * Poista luokan metodi tulosta
+ * ja luo luokalle metodi
+ * public String toString()
+ *
+ * joka palauttaa edellämainitun merkkijonoesityksen.
+ *
+ */
+ 
