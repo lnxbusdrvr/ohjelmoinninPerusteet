24bb676023cf467df954b685b1583cc2546d3e95
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-16 18:05:35 +0300

    tehtava

---
+++ moocJava2020/osa04/Osa04_16.Maksukortti/Paaohjelma.java
@@ -0,0 +1,23 @@
+/*
+ * Osa6: Monta korttia
+ * --------------------
+ *
+ * Tee luokan Paaohjelma main-metodiin koodi,
+ * joka sisältää seuraavan tapahtumasarjan:
+ *
+ * Luo Pekan kortti. Kortin alkusaldo on 20 euroa
+ * Luo Matin kortti. Kortin alkusaldo on 30 euroa
+ * Pekka syö maukkaasti
+ * Matti syö edullisesti
+ * Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+ * Pekka lataa rahaa 20 euroa
+ * Matti syö maukkaasti
+ * Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+ * Pekka syö edullisesti
+ * Pekka syö edullisesti
+ * Matti lataa rahaa 50 euroa
+ * Korttien arvot tulostetaan (molemmat omalle rivilleen, rivin alkuun kortin omistajan nimi)
+ *
+ */
+ 
+ 
