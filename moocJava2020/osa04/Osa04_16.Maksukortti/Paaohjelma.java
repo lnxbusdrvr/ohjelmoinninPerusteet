ac4145a9043fb871aa26bbb0712260b9922e51f9
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-16 18:36:08 +0300

    valmis

--- moocJava2020/osa04/Osa04_16.Maksukortti/Paaohjelma.java
+++ moocJava2020/osa04/Osa04_16.Maksukortti/Paaohjelma.java
@@ -21,3 +21,36 @@
  */
  
  
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        // Scanner lukija = new Scanner(System.in);
+        
+        // Tee tänne koodia jolla testaat että Maksukortti toimii halutulla tavalla
+        // muista kuitenkin pyyhkiä ylimääräinen koodi pois tehtävän viimeisessä osassa!
+        Maksukortti pekka = new Maksukortti(20);
+        Maksukortti matti = new Maksukortti(30);
+        
+        pekka.syoMaukkaasti();
+        matti.syoEdullisesti();
+        
+        System.out.println("Pekka "+pekka);
+        System.out.println("Matti "+matti);
+        
+        pekka.lataaRahaa(20);
+        matti.syoMaukkaasti();
+        
+        System.out.println("Pekka "+pekka);
+        System.out.println("Matti "+matti);
+        
+        pekka.syoEdullisesti();
+        pekka.syoEdullisesti();
+        
+        matti.lataaRahaa(50);
+        
+        System.out.println("Pekka "+pekka);
+        System.out.println("Matti "+matti);
+
+    }
+}
