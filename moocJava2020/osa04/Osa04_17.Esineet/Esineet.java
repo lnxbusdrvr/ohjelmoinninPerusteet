ff7a67c3d0114c82526df41ab3f5c1ea02aa734c
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-17 15:30:21 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_17.Esineet/Esineet.java
@@ -0,0 +1,39 @@
+/*
+ * Toteuta tässä kuvattu ohjelma luokkaan Esineet.
+ * Huom! Älä muuta luokkaa Esine.
+ *
+ * Kirjoita ohjelma,
+ * joka lukee käyttäjältä esineiden nimiä.
+ *
+ * Mikäli nimi on tyhjä, lopeta lukeminen.
+ * Mikäli nimi ei ole tyhjä,
+ * luo nimen perusteella uusi esine, jonka lisäät esineet-listalle.
+ *
+ * Tulosta tämän jälkeen esineet Esine-luokan toString-metodia hyödyntäen.
+ * Luokan Esine toteutus pitää syöttämäsi nimen lisäksi kirjaa esineen luomishetkestä.
+ *
+ * Ohjelman esimerkkitulostus:
+ *
+ * Nimi: Suo
+ * Nimi: Kuokka
+ * Nimi:
+ *
+ * Suo (luotu: 06.07.2018 12:34:56)
+ * Kuokka (luotu: 06.07.2018 12:34:57)
+ *
+ */
+ 
+ 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Esineet {
+
+    public static void main(String[] args) {
+        // toteuta luokkaa Henkilotieto käyttävä ohjelmasi tänne
+
+        ArrayList<Esine> esineet = new ArrayList<>();
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
