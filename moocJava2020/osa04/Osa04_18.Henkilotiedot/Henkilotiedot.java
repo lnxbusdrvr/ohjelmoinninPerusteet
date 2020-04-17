da330c5c2df38451fea53cee49fcc0c406437ce3
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-17 16:43:28 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_18.Henkilotiedot/Henkilotiedot.java
@@ -0,0 +1,49 @@
+/*
+ * Toteuta tässä kuvattu ohjelma luokkaan Henkilotiedot.
+ * Huom! Älä muuta luokkaa Henkilotieto.
+ *
+ * Kirjoita ohjelma, joka lukee käyttäjältä henkilötietoja.
+ *
+ * Käyttäjä syöttää etunimen, sukunimen, ja henkilötunnuksen.
+ *
+ * Mikäli etunimi on tyhjä, lopeta lukeminen.
+ * Mikäli etunimi ei ole tyhjä, lue loput tiedot ja luo käyttäjän syöttämistä tiedoista olio,
+ * jonka lisäät henkilotiedot-listalle.
+ *
+ * Kun käyttäjä on lopettanut tietojen syöttämisen (käyttäjä syöttää tyhjän etunimen),
+ * poistu toistolauseesta.
+ *
+ * Tulosta tämän jälkeen henkilötiedot siten,
+ * että jokaisesta syötetystä oliosta tulostetaan etunimi
+ * ja sukunimi välilyönnillä erotettuna (henkilötunnusta ei tulosteta!).
+ * Alla esimerkki ohjelman suorituksesta.
+ *
+ * Esimerkkitulostus:
+ *
+ * Etunimi: Jean
+ * Sukunimi: Bartik
+ * Henkilötunnus: 271224
+ * Etunimi: Betty
+ * Sukunimi: Holberton
+ * Henkilötunnus: 070317
+ * Etunimi:
+ *
+ * Jean Bartik
+ * Betty Holberton
+ *
+ */
+ 
+ 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Henkilotiedot {
+
+    public static void main(String[] args) {
+        // toteuta luokkaa Henkilotieto käyttävä ohjelmasi tänne
+
+        ArrayList<Henkilotieto> henkilotiedot = new ArrayList<>();
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
