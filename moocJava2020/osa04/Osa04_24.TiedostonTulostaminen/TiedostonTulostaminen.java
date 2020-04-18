e2cb6792269e02f6d6f4220862c039408929b514
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-18 21:16:14 +0300

    Koodausaika 6min 52sek

--- moocJava2020/osa04/Osa04_24.TiedostonTulostaminen/TiedostonTulostaminen.java
+++ moocJava2020/osa04/Osa04_24.TiedostonTulostaminen/TiedostonTulostaminen.java
@@ -12,12 +12,26 @@
  */
  
  
+
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class TiedostonTulostaminen {
 
     public static void main(String[] args) {
-
+        try{
+            Scanner tiedostoInput = new Scanner(Paths.get("data.txt"));
+            
+            // Luetaan tiedosto. Lopetetaan, kun tiedoston rivit loppuu
+            while(tiedostoInput.hasNextLine()) {
+                // Rivi kerrallaan
+                String rivi = tiedostoInput.nextLine();
+                
+                System.out.println(rivi);
+            }
+        // Mikäli syötevirhe
+        }catch(Exception e) {
+            System.out.println("Virhe"+e.getMessage());
+        }
     }
 }
