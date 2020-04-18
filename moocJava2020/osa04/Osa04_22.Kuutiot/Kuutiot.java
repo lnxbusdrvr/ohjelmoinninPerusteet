46023ede2b837b1f7b4b1c092cb5aad717c1b763
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-18 20:39:09 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_22.Kuutiot/Kuutiot.java
@@ -0,0 +1,20 @@
+/*
+ * Kirjoita ohjelma,
+ * joka lukee merkkijonoja käyttäjältä kunnes käyttäjä syöttää merkkijonon "loppu".
+ * Mikäli syöte ei ole "loppu",
+ * ohjelman tulee käsitellä syöte lukuna
+ * ja tulostaa syötetyn luvun kuutio (eli luku * luku * luku).
+ * Alla on muutamia tulostusesimerkkejä
+ *
+ * Esimerkkitulostus:
+ *
+ * 3
+ * 27
+ * -1
+ * -1
+ * 11
+ * 1331
+ * loppu
+ *
+ * Esimerkkitulostus:
+ * loppu
