c9e0952524eb87103605ca9354c763fcbcfb070a
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-18 20:58:26 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_24.TiedostonTulostaminen/TiedostonTulostaminen.java
@@ -0,0 +1,23 @@
+/*
+ * Kirjoita ohjelma, joka tulostaa tiedoston nimeltä "data.txt" sisällön siten,
+ * että kukin tiedoston rivi tulee tulostuksessa omalle rivilleen.
+ *
+ * Mikäli tiedoston sisältö on seuraava:
+ *
+ * Esimerkkidata:
+ *
+ * Olipa kerran
+ * maailma
+ *
+ */
+ 
+ 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class TiedostonTulostaminen {
+
+    public static void main(String[] args) {
+
+    }
+}
