cd2bc23733a54c0c55e557196be95584a4f9ccde
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-19 20:52:10 +0300

    Tehtävä

---
+++ moocJava2020/osa04/Osa04_26.VieraslistaTiedostosta/VieraslistaTiedostosta.java
@@ -0,0 +1,61 @@
+/*
+ * Tehtäväpohjassa on valmiina toiminnallisuus vieraslistaohjelmaan,
+ * missä käyttäjän syöttämien nimien olemassaolo tarkistetaan vieraslistalta.
+ * Ohjelmasta puuttuu kuitenkin toiminnallisuus vieraslistan lukemiseen.
+ * Muokkaa ohjelmaa siten, että vieraslistan nimet luetaan tiedostosta.
+ *
+ * Esimerkkitulostus:
+ *
+ * Minkä niminen tiedosto luetaan?
+ * vieraslista.txt
+ *
+ * Syötä nimiä, tyhjä rivi lopettaa.
+ * Chuck Norris
+ * Nimi ei ole listalla.
+ * Jack Baluer
+ * Nimi ei ole listalla.
+ * Jack Bauer
+ * Nimi on listalla.
+ * Jack Bower
+ * Nimi on listalla.
+ *
+ * Kiitos!
+ *
+ */
+ 
+ // Valmiina ollut koodi:
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VieraslistaTiedostosta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Minkä niminen tiedosto luetaan? ");
+        String tiedosto = lukija.nextLine();
+
+        ArrayList<String> lista = new ArrayList<>();
+        // toteuta tiedoston lukeminen tässä.
+        System.out.println("");
+
+        System.out.println("Syötä nimiä, tyhjä rivi lopettaa.");
+        while (true) {
+            String nimi = lukija.nextLine();
+            if (nimi.isEmpty()) {
+                break;
+            }
+
+            if (lista.contains(nimi)) {
+                System.out.println("Nimi on listalla.");
+            } else {
+                System.out.println("Nimi ei ole listalla.");
+            }
+        }
+
+        System.out.println("Kiitos!");
+    }
+}
+// Valmiina ollut koodi loppuu
