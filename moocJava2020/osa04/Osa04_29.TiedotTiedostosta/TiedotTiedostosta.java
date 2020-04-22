e04700c453b89eba38f8ebb834f2896e2f46cdc3
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-22 13:47:48 +0300

    Tehtävä
    Tässä commitissa oleva koodi oli jo valmiina.

---
+++ moocJava2020/osa04/Osa04_29.TiedotTiedostosta/TiedotTiedostosta.java
@@ -0,0 +1,39 @@
+/*
+ * Tässä tehtävässä käsitellään CSV-muodossa tallennettuja tiedostoja,
+ * jotka sisältävät riveittän pilkuilla eroteltuna nimiä ja ikiä.
+ * Tiedoston muoto on esimerkiksi seuraava:
+ *
+ * Esimerkkidata:
+ *
+ * lilja,3
+ * anton,5
+ * leevi,4
+ * aamu,1
+ *
+ * Tehtävänäsi on kirjoittaa ohjelma,
+ * joka ensin kysyy käyttäjältä luettavan tiedoston nimen.
+ * Tämän jälkeen ohjelma tulostaa tiedoston muotoiltuna
+ * seuraavalla tavalla (alla oletetaan,
+ * että tulostus tulee ylläolevasta tiedostosta):
+ *
+ * Esimerkkitulostus:
+ *
+ * Mikä tiedosto luetaan?
+ * data.txt
+ * lilja, ikä: 3 vuotta
+ * anton, ikä: 5 vuotta
+ * leevi, ikä: 4 vuotta
+ * aamu, ikä: 1 vuosi
+ *
+ */
+ 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class TiedotTiedostosta {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
