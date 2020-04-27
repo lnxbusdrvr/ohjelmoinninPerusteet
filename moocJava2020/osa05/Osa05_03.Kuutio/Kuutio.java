009475a551855c7f61530b8d25a8e0886008afc0
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-27 21:31:24 +0300

    Tehtävä

---
+++ moocJava2020/osa05/Osa05_03.Kuutio/Kuutio.java
@@ -0,0 +1,27 @@
+/*
+ * Luo kuutiota (eli säännöllistä kuusitahokasta) esittävä
+ * luokka Kuutio.
+ *
+ * Luo luokalle konstruktori
+ * public Kuutio(int sarmanPituus),
+ *
+ * joka saa parametrinaan kuution särmän pituuden.
+ *
+ * Tee kuutiolle metodi
+ * public int tilavuus(),
+ *
+ * joka laskee ja palauttaa kuution tilavuuden.
+ * Kuution tilavuus lasketaan
+ * kaavalla sarmanPituus * sarmanPituus * sarmanPituus.
+ *
+ * Tee tämän jälkeen kuutiolle vielä metodi
+ * public String toString(),
+ *
+ * joka palauttaa kuutiota kuvaavan merkkijonoesityksen.
+ * Merkkijonoesityksen tulee olla muotoa
+ * "Kuution särmän pituus on p, tilavuus on t",
+ *
+ * missä pon pituus ja t on tilavuus — 
+ * sekä pituus että tilavuus tulee kuvata kokonaislukuina.
+ *
+ */
