56c8fa73d81a179ddb02287daebf9fe3cb6d4446
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-29 10:01:13 +0300

    Tehtävä

---
+++ moocJava2020/osa05/Osa05_06.KuormitettuLaskuri/Laskuri.java
@@ -0,0 +1,31 @@
+/*
+ * Osa 1: Monta konstruktoria
+ *
+ * Toteuta luokka Laskuri,
+ * joka sisältää luvun, jota voi vähentää ja suurentaa.
+ * Luokalla tulee olla seuraavat konstruktorit:
+ * 
+ *  public Laskuri(int alkuarvo) asettaa laskurin alkuarvoksi parametrin alkuarvo arvon.
+ *  public Laskuri() laskurin alkuarvoksi tulee 0.
+ *
+ * ja seuraavat metodit:
+ *
+ *  public int arvo() palauttaa laskurin tämänhetkisen arvon
+ *  public void lisaa() lisää laskurin arvoa yhdellä
+ *  public void vahenna() vähentää laskurin arvoa yhdellä
+ *
+ * Osa2: Vaihtoehtoiset metodit
+ *
+ * Tee laskurin metodeista lisaa ja vahenna myös yksiparametriset versiot:
+ *
+ *  public void lisaa(int lisays) 
+ *
+ * lisää laskurin arvoa parametrina annetun luvun verran.
+ * Jos parametrin arvo on negatiivinen, ei laskurin arvo muutu.
+ *
+ *  public void vahenna(int vahennys)
+ *
+ * vähentää laskurin arvoa parametrina annetun luvun verran.
+ * Jos parametrin arvo on negatiivinen, ei laskurin arvo muutu.
+ *
+ */
