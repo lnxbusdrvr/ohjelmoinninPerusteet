d71a635134c6ab30a2d4806053228d2622637dda
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-29 10:11:33 +0300

    Koodausaika 5min 24sek

--- moocJava2020/osa05/Osa05_06.KuormitettuLaskuri/Laskuri.java
+++ moocJava2020/osa05/Osa05_06.KuormitettuLaskuri/Laskuri.java
@@ -29,3 +29,42 @@
  * Jos parametrin arvo on negatiivinen, ei laskurin arvo muutu.
  *
  */
+
+
+public class Laskuri {
+    private int alkuarvo;
+    
+    public Laskuri(int alkuarvo) {
+        this.alkuarvo = alkuarvo;
+    }
+    
+    public Laskuri() {
+        this.alkuarvo = 0;
+    }
+    
+    public int arvo() {
+        return this.alkuarvo;
+    }
+    
+    public void lisaa() {
+        this.alkuarvo = this.alkuarvo + 1;
+    }
+    
+    public void lisaa(int lisays) {
+        if(lisays < 0) {
+            lisays = 0;
+        }
+        this.alkuarvo = this.alkuarvo + lisays;
+    }
+    
+    public void vahenna() {
+        this.alkuarvo = this.alkuarvo - 1;
+    }
+    
+    public void vahenna(int vahennys) {
+        if(vahennys < 0) {
+            vahennys = 0;
+        }
+        this.alkuarvo = this.alkuarvo - vahennys;
+    }
+}
