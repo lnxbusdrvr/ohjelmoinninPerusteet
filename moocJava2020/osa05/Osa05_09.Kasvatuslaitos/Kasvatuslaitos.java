c4fecdb6643458aa926d23c348de4ee5b221e31e
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-30 16:45:14 +0300

    Tehtävä
    Tämä koodi oli jo valmiina

---
+++ moocJava2020/osa05/Osa05_09.Kasvatuslaitos/Kasvatuslaitos.java
@@ -0,0 +1,37 @@
+/*
+ * Osa 1: Henkilöiden punnitseminen
+ * --------------------------------
+ *
+ * Kasvatuslaitoksen luokkarungossa on valmiina runko metodille punnitse:
+ * 
+ * Metodi saa parametrina henkilön ja metodin on tarkoitus palauttaa 
+ * kutsujalleen parametrina olevan henkilön paino. 
+ * Paino selviää kutsumalla parametrina olevan 
+ * henkilön henkilo sopivaa metodia. Eli täydennä metodin koodi!
+ *
+ * Osa 2: Syöttäminen
+ * ------------------
+ *
+ * Tee kasvatuslaitokselle metodi public void syota(Henkilo henkilo) 
+ * joka kasvattaa parametrina olevan henkilön painoa yhdellä.
+ *
+ * Osa 3: Punnitusten laskeminen
+ * -----------------------------
+ *
+ * Tee kasvatuslaitokselle metodi public int punnitukset() 
+ * joka kertoo kuinka monta punnitusta kasvatuslaitos 
+ * on ylipäätään tehnyt. 
+ * Huom! Tarvitset uuden oliomuuttujan 
+ * punnitusten lukumäärän laskemiseen!
+ *
+ */
+ 
+ public class Kasvatuslaitos {
+
+
+    public int punnitse(Henkilo henkilo) {
+        // palautetaan parametrina annetun henkilön paino
+        return -1;
+    }
+
+}
