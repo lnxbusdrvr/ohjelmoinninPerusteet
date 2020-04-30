cd8c340e5e14baa2b8417495dec89511a9113637
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-04-30 22:38:26 +0300

    Koodausaika 7min 46sek

--- moocJava2020/osa05/Osa05_12.Kappale/Kappale.java
+++ moocJava2020/osa05/Osa05_12.Kappale/Kappale.java
@@ -19,11 +19,34 @@ public class Kappale {
         this.nimi = nimi;
         this.pituusSekunteina = pituusSekunteina;
     }
+    
+    public boolean equals(Object verrattava) {
+        // Tehdään tarvittavat toimenpiteet ennen vertailua
+        // jos muuttujat sijaitsevat samassa paikassa, ovat ne samat
+        if (this == verrattava) {
+            return true;
+        }
+
+        // jos verrattava olio ei ole Henkilo-tyyppinen, oliot eivät ole samat
+        if (!(verrattava instanceof Kappale)) {
+            return false;
+        }
+
+        // muunnetaan olio Kappale-olioksi
+        Kappale verrattavaKappale = (Kappale)verrattava;
+        
+        
+        if(this.esittaja.equals(verrattavaKappale.esittaja)
+                && this.nimi.equals(verrattavaKappale.nimi)
+        && this.pituusSekunteina == verrattavaKappale.pituusSekunteina) {
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     @Override
     public String toString() {
         return this.esittaja + ": " + this.nimi + " (" + this.pituusSekunteina + " s)";
     }
-
-
 }
