9cb46d54cf14e6668dfa9e7c4a1419d848a9a143
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-02 17:55:41 +0300

    Paaohjelman koodausaika 22min 47sek

--- moocJava2020/osa05/Osa05_15.KerailijanVarasto/Main.java
+++ moocJava2020/osa05/Osa05_15.KerailijanVarasto/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,7 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
-
+        ArrayList<Varasto> esineet = new ArrayList<>();
+        
+        while(true) {
+            System.out.println("Syötä esineen tunnus, tyhjä lopettaa.");
+            String tunnus = lukija.nextLine();
+            if(tunnus.equals("")) {
+                break;
+            }
+            
+            System.out.println("Syötä esineen nimi, tyhjä lopettaa.");
+            String nimi = lukija.nextLine();
+            if(nimi.equals("")) {
+                break;
+            }
+            Varasto esine = new Varasto(tunnus, nimi);
+            
+            if(esineet.contains(esine)) {
+            }else{
+                esineet.add(esine);
+            }
+        }
+        System.out.println("==Esineet==");
+        for(Varasto i : esineet) {
+            System.out.println(i);            
+        }
     }
 }
