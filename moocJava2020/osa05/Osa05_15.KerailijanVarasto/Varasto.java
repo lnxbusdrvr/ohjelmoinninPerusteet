894b094fc46d33714da2417f018f0bd221a9c018
Erkki Pokkinen <anticop@iki.fi>
2020-05-02 15:12:39 +0300

    Tehtävä

---
+++ moocJava2020/osa05/Osa05_15.KerailijanVarasto/Varasto.java
@@ -0,0 +1,25 @@
+/*
+ *
+ * Osa 1: Esineiden lisääminen ja listaaminen
+ * ------------------------------------------
+ *
+ * Ohjelman tulee lukea käyttäjältä esineitä. 
+ * Kun kaikki käyttäjän esineet on luettu, 
+ * ohjelma tulostaa esineiden tiedot. 
+ *
+ * Kustakin esineestä tulee lukea tunnus ja nimi. 
+ * Mikäli syötetty tunnus tai nimi on tyhjä, 
+ * ohjelma lopettaa syötteen pyytämisen ja tulostaa esineiden tiedot.
+ *
+ * Osa 2: Kukin esine tulostetaan vain kerran
+ *
+ * Muokkaa ohjelmaa siten, 
+ * että esineiden syöttämisen jälkeen kukin esine tulostetaan 
+ * korkeintaan kerran. 
+ * Kaksi esinettä tulee käsittää samoina mikäli niiden tunnukset 
+ * ovat samat (nimet voivat vaihdella esimerkiksi maittain). 
+ *
+ * Mikäli käyttäjä syöttää saman esineen useaan otteeseen, 
+ * tulostuksessa käytetään ensimmäisenä syötettyä esinettä.
+ *
+ */
