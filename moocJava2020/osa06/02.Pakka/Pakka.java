1f6355e74c0da282edae5ac7bff38130e4cc4520
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-04 21:46:06 +0300

    Osa 1/2 Koodausaika 5min 33sek

--- moocJava2020/osa06/02.Pakka/Pakka.java
+++ moocJava2020/osa06/02.Pakka/Pakka.java
@@ -31,3 +31,30 @@
  * ja poistaa sen pakasta.
  *
  */
+
+import java.util.ArrayList;
+
+public class Pakka {
+    private ArrayList<String> pakka;
+    
+    public Pakka() {
+        this.pakka = new ArrayList<>();
+    }
+    
+    public boolean onTyhja() {
+        if(this.pakka.isEmpty()) {
+            return true;
+        }else{
+            return false;
+        }        
+    }
+    
+    public void lisaa(String arvo) {
+        this.pakka.add(arvo);
+    }
+    
+    public ArrayList<String> arvot() {
+        return this.pakka;
+    }
+        
+}
