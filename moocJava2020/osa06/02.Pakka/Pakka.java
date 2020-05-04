b40a4f40a40bafb90875b8c7fe46275b9a44b0c9
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-04 21:27:53 +0300

    Tehtävä

---
+++ moocJava2020/osa06/02.Pakka/Pakka.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * Osa 1: Pakkaan lisääminen ja tyhjyyden tarkastaminen
+ * ----------------------------------------------------
+ *
+ * Luo luokka Pakka, 
+ * jolla on oliomuuttujana merkkijonoja sisältävä listan. 
+ * Lisää luokalle seuraavat metodit: 
+ *
+ * public boolean onTyhja() 
+ *
+ * palauttaa boolean-tyyppisen arvon (true tai false), 
+ * joka kertoo onko pakka tyhjä. 
+ *
+ * public void lisaa(String arvo) 
+ *
+ * lisää pakan päälle parametrina annetun arvon.
+ *
+ * public ArrayList<String> arvot() 
+ *
+ * palauttaa pakan sisältämän arvot listana.
+ *
+ * Osa 2: Pakasta ottaminen
+ * ------------------------
+ *
+ * Lisää luokalle Pakka metodi 
+ * public String ota(), 
+ *
+ * joka palauttaa pakan päällimmäisenä olevan arvon 
+ * (eli pakkaan viimeisenä lisätyn arvon) 
+ * ja poistaa sen pakasta.
+ *
+ */
