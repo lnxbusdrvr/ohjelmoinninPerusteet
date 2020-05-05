50defc28fbcf058b862a60ff9d0520a99863e53e
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-05 21:27:17 +0300

    Osa 1/2 Koodausaika 2min 47sek

---
+++ moocJava2020/osa06/05.Lahjapakkaamo/Lahja.java
@@ -0,0 +1,22 @@
+// Luokan koodausaika 2min 47sek
+public class Lahja {
+    private String nimi;
+    private int paino;
+    
+    public Lahja(String nimi, int paino) {
+        this.nimi = nimi;
+        this.paino = paino;
+    }
+    
+    public String getNimi() {
+        return this.nimi;
+    }
+    
+    public int getPaino() {
+        return this.paino;
+    }
+    
+    public String toString() {
+        return this.nimi+" ("+this.paino+" kg)";
+    }
+}
