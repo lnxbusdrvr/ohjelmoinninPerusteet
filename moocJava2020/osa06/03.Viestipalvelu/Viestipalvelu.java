15551b1e3785ad1993e3176e3feceaf6e7e030ea
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-05 16:52:53 +0300

    Koodausaika 10min 42sek

--- moocJava2020/osa06/03.Viestipalvelu/Viestipalvelu.java
+++ moocJava2020/osa06/03.Viestipalvelu/Viestipalvelu.java
@@ -21,3 +21,23 @@
  * liittyvällä length()-metodilla.
  *
  */
+
+import java.util.ArrayList;
+
+public class Viestipalvelu {
+    private ArrayList<Viesti> lista;
+    
+    public Viestipalvelu() {
+        this.lista = new ArrayList<>();
+    }
+    
+    public void lisaa(Viesti viesti) {
+        if(viesti.getPituus() <= 280) {
+            lista.add(viesti);
+        }
+    }
+    
+    public ArrayList<Viesti> getViestit() {
+        return this.lista;
+    }
+}
