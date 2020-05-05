8e9d08fe35a27991a7d2104555bb4a5bd2f694f6
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-05-05 15:55:19 +0300

    Tehtävä

---
+++ moocJava2020/osa06/03.Viestipalvelu/Viestipalvelu.java
@@ -0,0 +1,23 @@
+/*
+ * Tehtäväpohjassa on valmiina luokka Viesti, 
+ * jonka avulla voidaan luoda viestejä kuvaavia olioita. i
+ * Jokaisella viestillä on lähettäjä ja sisältö. 
+ *
+ * Toteuta luokka Viestipalvelu. 
+ * Luokalla tulee olla parametriton konstruktori ja sen tulee 
+ * sisältää lista Viesti-olioita.
+ * Lisää luokalle tämän jälkeen seuraavat kaksi metodia: 
+ *
+ * public void lisaa(Viesti viesti) 
+ *
+ * lisää viestipalveluun parametrina annetun viestin mikäli viestin 
+ * sisällön pituus on korkeintaan 280 merkkiä. 
+ *
+ * public ArrayList<Viesti> getViestit() 
+ *
+ * palauttaa viestipaveluun lisätyt viestit. 
+ *
+ * Vinkki! Saat merkkijonon pituuden merkkijonoon i
+ * liittyvällä length()-metodilla.
+ *
+ */
