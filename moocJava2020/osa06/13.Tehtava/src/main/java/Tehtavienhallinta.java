10988f82917fd07b1ce14831487b8dba0dc059e9
Erkki Pokkinen <anticop@iki.fi>
2020-05-24 21:41:20 +0300

    Askel 6: Kolmas toiminnallisuus

--- moocJava2020/osa06/13.Tehtava/src/main/java/Tehtavienhallinta.java
+++ moocJava2020/osa06/13.Tehtava/src/main/java/Tehtavienhallinta.java
@@ -3,13 +3,19 @@ import java.util.ArrayList;
 
 public class Tehtavienhallinta {
     
+    private ArrayList<String> tehtavat;
+    
+    public Tehtavienhallinta() {
+        this.tehtavat = new ArrayList<>();
+    }
+    
     public ArrayList<String> tehtavalista() {
-        return new ArrayList<>();
-        
+        return this.tehtavat;
     }
     
     public void lisaa(String tehtava) {
-        
+        this.tehtavat.add(tehtava);
     }
 
 }
+
