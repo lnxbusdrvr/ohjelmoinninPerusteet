903c76a7170a8aacde1df9756b5d5d6a862a3a37
Erkki Pokkinen <anticop@iki.fi>
2020-05-24 21:07:48 +0300

    Askel 1: Tee testi

--- moocJava2020/osa06/13.Tehtava/src/test/java/TehtavienhallintaTest.java
+++ moocJava2020/osa06/13.Tehtava/src/test/java/TehtavienhallintaTest.java
@@ -1,7 +1,20 @@
 
-
+// Haetaan nämä kirjastot testaamista varten
+import static org.junit.Assert.*;
+import org.junit.Test; // Huom ei static:
 
 public class TehtavienhallintaTest {
-
+    
+    // Tässä metodia käytetään tehtävälistan koon
+    // tarkastamiseen uuden tehtävälistan luomisen 
+    // yhteydessä: uuden listan tulee olla tyhjä.
+    @Test
+    public void TehtavienhallintaTest() {
+        Tehtavienhallinta hallinta = new Tehtavienhallinta();
+        // ensimmäinen(nolla) on odotettu arvo ja 
+        // toinen on ohjelman palauttama arvo. .
+        assertEquals(0, hallinta.tehtavalista().size());
+    }
+    
 }
 
