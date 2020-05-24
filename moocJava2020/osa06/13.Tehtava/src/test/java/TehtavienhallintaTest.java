e83918fe6b1036bdba7efe6cf827510b7ef7afe9
Erkki Pokkinen <anticop@iki.fi>
2020-05-24 22:13:35 +0300

    Refaktorointi

--- moocJava2020/osa06/13.Tehtava/src/test/java/TehtavienhallintaTest.java
+++ moocJava2020/osa06/13.Tehtava/src/test/java/TehtavienhallintaTest.java
@@ -1,30 +1,19 @@
 
-// Haetaan nämä kirjastot testaamista varten
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test; // Huom ei static:
 
 public class TehtavienhallintaTest {
     
-    // Tässä metodia käytetään tehtävälistan koon
-    // tarkastamiseen uuden tehtävälistan luomisen 
-    // yhteydessä: uuden listan tulee olla tyhjä.
     @Test
-    public void TehtavienhallintaTest() {
+    public void tehtavalistaAlussaTyhja() {
         Tehtavienhallinta hallinta = new Tehtavienhallinta();
-        // ensimmäinen(nolla) on odotettu arvo ja 
-        // toinen on ohjelman palauttama arvo. .
         assertEquals(0, hallinta.tehtavalista().size());
     }
     
-    // Testissä määritellään luokalle Tehtavienhallinta metodi lisaa, 
-    // joka lisää tehtävälistalle uuden tehtävän. 
-    // Tehtävän lisäämisen onnistuminen tarkastetaan tehtavalista-metodin 
-    // koon kasvamisen kautta.
     @Test
     public void tehtavanLisaaminenKasvattaaListanKokoaYhdella() {
         Tehtavienhallinta hallinta = new Tehtavienhallinta();
-        // Täydennetään testejä siten, että ne vaativat, 
-        // että lisätyn tehtävän tulee olla listalla. 
         hallinta.lisaa("Kirjoita testi");
         assertEquals(1, hallinta.tehtavalista().size());
     }
@@ -33,10 +22,7 @@ public class TehtavienhallintaTest {
     public void lisattyTehtavaLoytyyTehtavalistalta() {
         Tehtavienhallinta hallinta = new Tehtavienhallinta();
         hallinta.lisaa("Kirjoita testi");
-        // assertTrue vaatii, että sille parametrina annettu lauseke 
-        // saa lopulta arvon true.
         assertTrue(hallinta.tehtavalista().contains("Kirjoita testi"));
     }
     
 }
-
