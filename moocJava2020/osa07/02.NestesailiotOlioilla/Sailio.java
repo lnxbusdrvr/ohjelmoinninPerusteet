e605f97140241caada98be516f4984c286fbae13
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-07-12 13:06:14 +0300

    Osa 1/2 Koodausaika 7min 2sek

---
+++ moocJava2020/osa07/02.NestesailiotOlioilla/Sailio.java
@@ -0,0 +1,44 @@
+
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Sailio {
+    
+    private int sisalto = 0;
+    
+    public Sailio() {
+        
+    }
+    
+    public int sisalto() {
+        return this.sisalto;
+    }
+    
+    public void lisaa(int maara) {
+        if(maara > 0) {
+            this.sisalto += maara;
+            if(this.sisalto > 0) {
+                
+            }
+            if(this.sisalto > 100) {
+                this.sisalto = 100;
+            }
+        }
+    }
+    
+    public void poista(int maara) {
+        if(maara > 0) {
+            sisalto -= maara;
+            if(this.sisalto < 0) {
+                this.sisalto = 0;
+            }
+        }
+    }
+    
+    public String toString() {
+        return this.sisalto+"/100";
+    }
+    
+}
