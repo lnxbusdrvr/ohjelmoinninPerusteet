30afc382c2d404bb9d29d19e16e381344f9e2cc2
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-07-15 00:00:57 +0300

    Koodausaika 6min 33sek
    Ajatusaika 10min 16sek

---
+++ moocJava2020/osa07/04.ValmiitJarjestamisalgoritmit/Paaohjelma.java
@@ -0,0 +1,48 @@
+/*
+ * 07_04.ValmiitJarjestamisalgoritmit
+ *
+ * Lisää luokkaan Paaohjelma seuraavat luokkametodit:
+ * - public static void jarjesta(int[] taulukko) 
+ *      järjestää kokonaislukutaulukon.
+ * - public static void jarjesta(String[] taulukko)
+ *      järjestää merkkijonotaulukon.
+ * - public static void jarjestaLuvut(ArrayList<Integer> luvut)
+ *      järjestää lukuja sisältävän listan.
+ * - public static void jarjestaMerkkijonot(ArrayList<String> merkkijonot)
+ *      järjestää merkkijonoja sisältävän listan.
+ *
+ * Hyödynnä metodien toteutuksessa Javan valmiita kirjastoja.
+ *
+ */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        // tee testikoodia tänne
+        int[] taulukko = {3, 1, 5, 99, 3, 12};
+        
+    }
+    
+    public static void jarjesta(int[] taulukko) {
+        Arrays.sort(taulukko);
+    }
+    
+    public static void jarjesta(String[] taulukko) {
+        Arrays.sort(taulukko);
+    }
+    
+    public static void jarjestaLuvut(ArrayList<Integer> luvut) {
+        Collections.sort(luvut);
+    }
+    
+    public static void jarjestaMerkkijonot(ArrayList<String> merkkijonot) {
+        Collections.sort(merkkijonot);
+    }
+
+}
