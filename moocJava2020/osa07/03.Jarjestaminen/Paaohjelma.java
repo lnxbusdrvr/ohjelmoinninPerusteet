e9a7aeae36e5a9171b62f3c6820a705a968fff7c
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-07-14 20:04:45 +0300

    Osa 1/5 Koodausaika 15min

--- moocJava2020/osa07/03.Jarjestaminen/Paaohjelma.java
+++ moocJava2020/osa07/03.Jarjestaminen/Paaohjelma.java
@@ -5,5 +5,17 @@ public class Paaohjelma {
         // tee testikoodia tänne
         int[] taulukko = {3, 1, 5, 99, 3, 12};
     }
-
+    
+    public static int pienin(int[] taulukko) {
+        
+        int pienin = taulukko[0];
+        
+        for(int i : taulukko) {
+            if(i<pienin) {
+                pienin = i;
+            }
+        }
+        return pienin;        
+    }
+    
 }
