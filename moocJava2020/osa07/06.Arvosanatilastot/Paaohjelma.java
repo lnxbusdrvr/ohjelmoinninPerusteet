4d23d2f58dc1fd55800cc5ea6b7b0be6f3215b83
Erkki Pokkinen <lnxbusdrvr@gmail.com>
2020-07-21 20:16:40 +0300

    Osa 1/4 Koodausaika 4min 47sek

--- moocJava2020/osa07/06.Arvosanatilastot/Paaohjelma.java
+++ moocJava2020/osa07/06.Arvosanatilastot/Paaohjelma.java
@@ -8,5 +8,27 @@ public class Paaohjelma {
 
         // Tee sovelluksesi tänne -- kannattaa harkita sovelluksen pilkkomista
         // useampaan luokkaan.
+        int lukumaara = 0;
+        int summa = 0;
+        
+        
+        System.out.println("Syötä yhteispisteet, -1 lopettaa:");
+
+        while(true) {
+            int luku = Integer.valueOf(lukija.nextLine());
+
+            if(luku == -1) {
+                break;
+            }
+            if(luku > 0 && luku <=100) {
+                summa = summa + luku;
+                lukumaara++;
+            }/*else{
+                lukumaara--;
+            }*/
+            
+        }
+        double keskiarvo = (double)summa/lukumaara;
+        System.out.println("Pisteiden keskiarvo (kaikki): "+keskiarvo);
     }
 }
