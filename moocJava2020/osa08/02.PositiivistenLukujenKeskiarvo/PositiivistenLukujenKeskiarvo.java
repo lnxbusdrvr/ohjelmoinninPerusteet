150a25b00b979f3d500fa1dc5b18643395516c7d
Erkki Pokkinen <anticop@iki.fi>
2020-07-26 15:16:53 +0300

    Koodausaika 11min 23sek

--- moocJava2020/osa08/02.PositiivistenLukujenKeskiarvo/PositiivistenLukujenKeskiarvo.java
+++ moocJava2020/osa08/02.PositiivistenLukujenKeskiarvo/PositiivistenLukujenKeskiarvo.java
@@ -28,6 +28,34 @@ public class PositiivistenLukujenKeskiarvo {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        
+        int lkm = 0;
+        int summa = 0;
+        double keskiarvo = 0;
+        
+        while(true) {
+            int luku = Integer.parseInt(lukija.nextLine());
+            
+            if(luku == 0) {
+                break;
+            }
+            if(luku > 0) {
+                lkm++;
+                summa += luku;
+            }
+        }
+        
+        if(lkm == 0) {
+            System.out.println("keskiarvon laskeminen ei ole mahdollista");
+        }else{
+            // ilman double-käännöstä
+            // luku pyöristyy
+            keskiarvo = (double)summa / lkm;
+            // Debug
+            //System.out.println("Summa: "+summa);
+            //System.out.println("Lkm: "+lkm);
+            System.out.println(keskiarvo);
+        }
 
     }
 }
