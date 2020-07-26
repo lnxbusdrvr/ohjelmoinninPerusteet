d4577ccdeaeb1003bad45bc6d76aa95bc6573fcc
Erkki Pokkinen <anticop@iki.fi>
2020-07-26 16:26:07 +0300

    Valmis pohja ja tehtävänanto

---
+++ moocJava2020/osa08/06.Lempinimet/Lempinimet.java
@@ -0,0 +1,24 @@
+/*
+ *
+ * Luo main-metodissa uusi HashMap<String,String>-olio. i
+ * Tallenna luomaasi olioon seuraavien henkilöiden nimet 
+ * ja lempinimet niin, että nimi on avain 
+ * ja lempinimi on arvo. Käytä pelkkiä pieniä kirjaimia.
+ *
+ * matin lempinimi on mage
+ * mikaelin lempinimi on mixu 
+ * arton lempinimi on arppa 
+ *
+ * Tämän jälkeen hae HashMapistä mikaelin lempinimi ja tulosta se. 
+ *
+ */
+
+import java.util.HashMap;
+
+public class Lempinimet {
+
+    public static void main(String[] args) {
+        // Tee tehtävänannossa pyydetyt operaatiot täällä!
+    }
+
+}
