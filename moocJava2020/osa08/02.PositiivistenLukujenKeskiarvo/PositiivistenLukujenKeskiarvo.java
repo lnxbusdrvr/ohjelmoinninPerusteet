418bed3609e361269412094ff615a75649931456
Erkki Pokkinen <anticop@iki.fi>
2020-07-26 14:40:13 +0300

    Valmis pohja ja tehtävä

---
+++ moocJava2020/osa08/02.PositiivistenLukujenKeskiarvo/PositiivistenLukujenKeskiarvo.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * Kirjoita ohjelma, 
+ * joka lukee käyttäjältä kokonaislukuja 
+ * kunnes käyttäjä syöttää luvun 0. 
+ * Tämän jälkeen ohjelma tulostaa 
+ * syötteessä esiintyneiden 
+ * positiivisten (eli nollaa suurempien lukujen) keskiarvon.
+ * 
+ * Mikäli ohjelmassa ei syötetä yhtäkään positiivista lukua, 
+ * ohjelman tulee 
+ * tulostaa "keskiarvon laskeminen ei ole mahdollista".
+ *
+ * Esimerkkitulostus:
+ *
+ *  3 
+ *  5 
+ *  1 
+ * -3 
+ *  0 
+ *  3.0
+ *
+ */
+
+import java.util.Scanner;
+
+public class PositiivistenLukujenKeskiarvo {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
