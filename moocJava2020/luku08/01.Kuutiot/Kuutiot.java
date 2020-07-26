cb0d34eeee1d76ce1f84dcf16ca4144b2a53cb56
Erkki Pokkinen <anticop@iki.fi>
2020-07-26 14:14:50 +0300

    Koodausaika 5min 6sek

--- moocJava2020/luku08/01.Kuutiot/Kuutiot.java
+++ moocJava2020/luku08/01.Kuutiot/Kuutiot.java
@@ -17,12 +17,25 @@
  * loppu
  *
  */
+
 import java.util.Scanner;
 
 public class Kuutiot {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        
+        while(true) {
+            String syote = lukija.nextLine();
+            
+            if(syote.equals("loppu")) {
+                break;
+            }else{
+                int luku = Integer.parseInt(syote);
+                System.out.println((luku*luku*luku));
+            }
+            
+        }
 
     }
 }
