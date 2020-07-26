e594d21121b1e09d55929cdc0d6e080f8ea0ce11
Erkki Pokkinen <anticop@iki.fi>
2020-07-26 13:58:34 +0300

    Valmis pohja ja tehtävä

---
+++ moocJava2020/luku08/01.Kuutiot/Kuutiot.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * Kirjoita ohjelma, 
+ * joka lukee merkkijonoja käyttäjältä kunnes käyttäjä syöttää merkkijonon "loppu". 
+ * Mikäli syöte ei ole "loppu", 
+ * ohjelman tulee käsitellä syöte lukuna 
+ * ja tulostaa syötetyn luvun kuutio (eli luku * luku * luku). 
+ *
+ * Esimerkkitulostus:
+ *
+ * 3 
+ * 27 
+ * -1 
+ * -1 
+ * 11 
+ * 1331 
+ * loppu
+ *
+ */
+import java.util.Scanner;
+
+public class Kuutiot {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
