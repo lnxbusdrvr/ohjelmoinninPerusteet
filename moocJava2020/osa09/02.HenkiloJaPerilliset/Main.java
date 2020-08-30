4ffbac1cbea7b4ef026020e2230530cb8afac674
Erkki Pokkinen <anticop@iki.fi>
2020-08-30 21:12:03 +0300

    Osa 2/5 Koodausaika 3min 30sek

--- moocJava2020/osa09/02.HenkiloJaPerilliset/Main.java
+++ moocJava2020/osa09/02.HenkiloJaPerilliset/Main.java
@@ -1,21 +1,24 @@
 
 
-/**
- *
- * @author lnxbusdrvr
- */
-public class Henkilo {
-    
-    private String nimi;
-    private String osoite;
-    
-    public Henkilo(String nimi, String osoite) {
-        this.nimi = nimi;
-        this.osoite = osoite;
+public class Main {
+
+    public static void main(String[] args) {
+        // kirjoita tänne testikoodia
+        
+        // Osa 1
+        Henkilo ada = new Henkilo("Ada Lovelace", "Korsontie 1 03100 Vantaa");
+        Henkilo esko = new Henkilo("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+        System.out.println(ada);
+        System.out.println(esko);
+        
+        
+        // Osa 2
+        Opiskelija olli = new Opiskelija("Olli", "Ida Albergintie 1 00400 Helsinki");
+        System.out.println(olli);
+        System.out.println("opintopisteitä " + olli.opintopisteita());
+        olli.opiskele();
+        System.out.println("opintopisteitä "+ olli.opintopisteita());
+        
     }
-    
-    public String toString() {
-        return this.nimi+"\n  "+this.osoite;
-    }
-    
+
 }
---
+++ moocJava2020/osa09/02.HenkiloJaPerilliset/Opiskelija.java
@@ -0,0 +1,23 @@
+
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Opiskelija extends Henkilo {
+    private int opintopisteet;
+    
+    public Opiskelija(String nimi, String osoite) {
+        super(nimi, osoite);
+        this.opintopisteet = 0;
+    }
+    
+    public void opiskele() {
+        this.opintopisteet++;
+    }
+    
+    public int opintopisteita() {
+        return this.opintopisteet;
+    }
+    
+}
