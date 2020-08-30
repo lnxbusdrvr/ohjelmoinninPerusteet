638d83818b6b895c745131805443592ef7488b05
Erkki Pokkinen <anticop@iki.fi>
2020-08-30 22:53:11 +0300

    Osa 4/5 Koodausaika 2min 54sek

--- moocJava2020/osa09/02.HenkiloJaPerilliset/Main.java
+++ moocJava2020/osa09/02.HenkiloJaPerilliset/Main.java
@@ -6,22 +6,39 @@ public class Main {
         // kirjoita tänne testikoodia
         
         // Osa 1
+        /*
         Henkilo ada = new Henkilo("Ada Lovelace", "Korsontie 1 03100 Vantaa");
         Henkilo esko = new Henkilo("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
         System.out.println(ada);
-        System.out.println(esko);
+        System.out.println(esko);*/
                 
         // Osa 2
+        /*
         Opiskelija olli = new Opiskelija("Olli", "Ida Albergintie 1 00400 Helsinki");
         System.out.println(olli);
         System.out.println("opintopisteitä " + olli.opintopisteita());
         olli.opiskele();
-        System.out.println("opintopisteitä "+ olli.opintopisteita());
+        System.out.println("opintopisteitä "+ olli.opintopisteita());*/
         
         // Osa 3
-        //Opiskelija olli = new Opiskelija("Olli", "Ida Albergintie 1 00400 Helsinki");
+        /*Opiskelija olli = new Opiskelija("Olli", "Ida Albergintie 1 00400 Helsinki");
         System.out.println(olli);
         olli.opiskele();
+        System.out.println(olli);*/
+        
+        // Osa 4
+        Opettaja ada = new Opettaja("Ada Lovelace", "Korsontie 1 03100 Vantaa", 1200);
+        Opettaja esko = new Opettaja("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        System.out.println(ada);
+        System.out.println(esko);
+
+        Opiskelija olli = new Opiskelija("Olli", "Ida Albergintie 1 00400 Helsinki");
+
+        int i = 0;
+        while (i < 25) {
+            olli.opiskele();
+            i = i + 1;
+        }
         System.out.println(olli);
 
     }
