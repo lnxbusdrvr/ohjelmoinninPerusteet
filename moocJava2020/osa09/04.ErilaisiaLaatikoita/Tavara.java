d4abfb4fa5623447debf26edfdfac196e4a789e3
Erkki Pokkinen <anticop@iki.fi>
2020-09-06 18:49:28 +0300

    Osa 1/3 ei koodausaikaa

--- moocJava2020/osa09/04.ErilaisiaLaatikoita/Tavara.java
+++ moocJava2020/osa09/04.ErilaisiaLaatikoita/Tavara.java
@@ -1,4 +1,7 @@
 
+import java.util.Objects;
+
+
 public class Tavara {
 
     private String nimi;
@@ -20,5 +23,32 @@ public class Tavara {
     public int getPaino() {
         return paino;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.nimi);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tavara other = (Tavara) obj;
+        if (!Objects.equals(this.nimi, other.nimi)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 
 }
