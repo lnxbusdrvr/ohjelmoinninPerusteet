4ec8f803769104048feabbed4c58d0841efc6bd6
Erkki Pokkinen <anticop@iki.fi>
2020-09-12 12:52:50 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa09/09.SetMetodinParametrina/Paaohjelma.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * Tehtävä: 09.SetMetodinParametrina
+ * ---------------------------------
+ * Toteuta pääohjelmaluokkaan luokkametodi palautaKoko, joka saa 
+ * parametrina Set-olion ja palauttaa sen koon kokonaislukuna.
+ *
+ * Metodin tulee toimia esimerkiksi seuraavasti:
+ *
+ * Set<String> nimet = new HashSet<>();
+ * nimet.add("eka");
+ * nimet.add("eka");
+ * nimet.add("toka");
+ * nimet.add("toka");
+ * nimet.add("toka");
+ *
+ * Esimerkkitulostus:
+ *
+ * 2
+ *
+ */
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        // tee tänne testikoodia
+
+    }
+
+    // toteuta tänne metodi palautaKoko, joka palauttaa parametrina 
+    // saamansa set-olion alkioiden lukumäärän
+
+}
