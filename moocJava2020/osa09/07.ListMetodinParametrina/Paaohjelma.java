121abdc8c6ddfa8916725e9c80a87dddf7332a0c
Erkki Pokkinen <anticop@iki.fi>
2020-09-12 11:50:05 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa09/07.ListMetodinParametrina/Paaohjelma.java
@@ -0,0 +1,32 @@
+/*
+ *  
+ * Tehtävä: 07.ListMetodinParametrina
+ * ----------------------------------
+ * Toteuta pääohjelmaluokkaan luokkametodi palautaKoko, 
+ * joka saa parametrina List-olion ja palauttaa sen koon kokonaislukuna.
+ *
+ * Metodin tulee toimia esimerkiksi seuraavasti:
+ *
+ * List<String> nimet = new ArrayList<>();
+ * nimet.add("eka");
+ * nimet.add("toka");
+ * nimet.add("kolmas");
+ *
+ * System.out.println(palautaKoko(nimet));
+ *
+ * Esimerkkitulostus:
+ * 
+ * 3
+ *
+ */
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        // tee tänne testikoodia
+
+    }
+
+    // toteuta tänne  metodi palautaKoko, joka palauttaa parametrina 
+    // saamansa lista-olion alkioiden lukumäärän
+}
