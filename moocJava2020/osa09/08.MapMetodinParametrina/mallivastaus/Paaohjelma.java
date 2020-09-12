ecb153239ce474040bc8683b50b4fb94521741c6
Erkki Pokkinen <anticop@iki.fi>
2020-09-12 12:24:38 +0300

    Mallivastaus sama, kuin oma

---
+++ moocJava2020/osa09/08.MapMetodinParametrina/mallivastaus/Paaohjelma.java
@@ -0,0 +1,16 @@
+ 
+import java.util.Map;
+ 
+public class Paaohjelma {
+ 
+    public static void main(String[] args) {
+        // tee tänne testikoodia
+ 
+    }
+ 
+    // toteuta tänne metodi palautaKoko, joka palauttaa parametrina 
+    // saamansa map-olion alkioiden lukumäärän
+    public static int palautaKoko(Map map) {
+        return map.size();
+    }
+}
