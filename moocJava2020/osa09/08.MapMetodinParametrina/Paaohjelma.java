beec3ae5eced348b6b907698c09653d75c461a6a
Erkki Pokkinen <anticop@iki.fi>
2020-09-12 12:19:30 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa09/08.MapMetodinParametrina/Paaohjelma.java
@@ -0,0 +1,32 @@
+/*
+ *
+ * 08.MapMetodinParametrina
+ * ------------------------
+ * Toteuta pääohjelmaluokkaan luokkametodi palautaKoko, 
+ * joka saa parametrina Map-olion ja palauttaa sen 
+ * koon kokonaislukuna.
+ *
+ * Metodin tulee toimia esimerkiksi seuraavasti:
+ *
+ * Map<String, String> nimet = new HashMap<>();
+ * nimet.put("eka", "first");
+ * nimet.put("toka", "second");
+ *
+ * System.out.println(palautaKoko(nimet));
+ *
+ * Esimerkkitulostus:
+ *
+ * 2
+ *
+ */
+
+public class Paaohjelma {
+
+    public static void main(String[] args) {
+        // tee tänne testikoodia
+
+    }
+
+    // toteuta tänne metodi palautaKoko, joka palauttaa parametrina 
+    // saamansa map-olion alkioiden lukumäärän
+}
