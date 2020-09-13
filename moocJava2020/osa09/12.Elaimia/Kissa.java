09ea2b5b8b4e9de5a30bdff76f3a5c6cdb8d2e2b
Erkki Pokkinen <anticop@iki.fi>
2020-09-13 12:15:41 +0300

    Osa 3/4 Koodausaika 1min 26sek

---
+++ moocJava2020/osa09/12.Elaimia/Kissa.java
@@ -0,0 +1,25 @@
+
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Kissa extends Elain{
+
+    public Kissa(String nimi) {
+        super(nimi);
+    }
+
+    public Kissa() {
+        super("Kissa");
+    }
+    
+    public void mourua() {
+        System.out.println(super.getNimi()+" mouruaa");
+    }
+    
+    
+    
+    
+    
+}
--- moocJava2020/osa09/12.Elaimia/Main.java
+++ moocJava2020/osa09/12.Elaimia/Main.java
@@ -4,13 +4,22 @@ public class Main {
     public static void main(String[] args) {
         // voit kokeilla luomiesi luokkien toimintaa täällä
         
-        // Osa 2
+        /* Osa 2
         Koira koira = new Koira();
         koira.hauku();
         koira.syo();
 
         Koira vuffe = new Koira("Vuffe");
         vuffe.hauku();
+        */
+        
+        // Osa 3
+        Kissa kissa = new Kissa();
+        kissa.mourua();
+        kissa.syo();
+
+        Kissa karvinen = new Kissa("Karvinen");
+        karvinen.mourua();
 
     }
 
