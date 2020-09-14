d567e34d9fbb460d6e465c76b2483689e45754d3
Erkki Pokkinen <anticop@iki.fi>
2020-09-14 17:22:18 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/01.LukujenKeskiarvo/LukujenKeskiarvo.java
@@ -0,0 +1,42 @@
+/*
+ *
+ * Tehtävä: 01.LukujenKeskiarvo
+ * ----------------------------
+ * Toteuta ohjelma, joka lukee käyttäjältä syötteitä. 
+ * Jos käyttäjä syöttää merkkijonon "loppu", lukeminen lopetetaan. 
+ * Muut syötteet ovat lukuja. Kun käyttäjä syöttää merkkijonon "loppu", 
+ * ohjelman tulee tulostaa syötettyjen lukujen keskiarvo. 
+ *
+ * Toteuta keskiarvon laskeminen virran avulla!
+ * 
+ * Esimerkkitulostus:
+ *
+ * Kirjoita syötteitä, "loppu" lopettaa.
+ * 2
+ * 4
+ * 6
+ * loppu
+ * Lukujen keskiarvo: 4.0
+ *
+ * Esimerkkitulostus:
+ * 
+ * Kirjoita syötteitä, "loppu" lopettaa.
+ * -1
+ * 1
+ * 2
+ * loppu
+ * Lukujen keskiarvo: 0.6666666666666666
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LukujenKeskiarvo {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // toteuta ohjelmasi tänne
+
+    }
+}
