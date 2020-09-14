111df19381bd08c187f2de83a39f2d311001cd46
Erkki Pokkinen <anticop@iki.fi>
2020-09-14 17:36:07 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/02.TiettyjenLukujenKeskiarvo/TiettyjenLukujenKeskiarvo.java
@@ -0,0 +1,54 @@
+/*
+ *
+ * Tehtävä: 02.TiettyjenLukujenKeskiarvo
+ * -------------------------------------
+ * Toteuta ohjelma, joka lukee käyttäjältä syötteitä. Jos käyttäjä 
+ * syöttää merkkijonon "loppu", 
+ * lukeminen lopetetaan. 
+ * Muut syötteet ovat lukuja. Kun käyttäjä syöttää merkkijonon "loppu", 
+ * syötteiden lukeminen lopetetaan.
+ *
+ * Tämän jälkeen käyttäjältä kysytään tulostetaanko negatiivisten vai 
+ * positiivisten lukujen keskiarvo (n vai p). Jos käyttäjä syöttää 
+ * merkkijonon "n", tulostetaan negatiivisten lukujen keskiarvo, 
+ * muulloin tulostetaan positiivisten lukujen keskiarvo.
+ *
+ * Toteuta keskiarvon laskeminen sekä rajaus virran avulla!
+ *
+ * Esimerkkitulostus:
+ *
+ * Kirjoita syötteitä, "loppu" lopettaa.
+ * -1
+ * 1
+ * 2
+ * loppu
+ *
+ * Tulostetaanko negatiivisten vai positiivisten lukujen keskiarvo? (n/p)
+ * n
+ * Negatiivisten lukujen keskiarvo: -1.0
+ *
+ * Esimerkkitulostus:
+ *
+ * Kirjoita syötteitä, "loppu" lopettaa.
+ * -1
+ * 1
+ * 2
+ * loppu
+ *
+ * Tulostetaanko negatiivisten vai positiivisten lukujen keskiarvo? (n/p)
+ * p
+ * Positiivisten lukujen keskiarvo: 1.5
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TiettyjenLukujenKeskiarvo {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // toteuta ohjelmasi tänne
+
+    }
+}
