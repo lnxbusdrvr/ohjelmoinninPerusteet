5685d5a0dc36248fefc1aaa13876791f82cc3191
Erkki Pokkinen <anticop@iki.fi>
2020-09-15 17:51:12 +0300

    Koodausaika 3min 5sek

--- moocJava2020/osa10/02.TiettyjenLukujenKeskiarvo/..03.PositiivisetLuvut/PositiivisetLuvut.java
+++ moocJava2020/osa10/02.TiettyjenLukujenKeskiarvo/..03.PositiivisetLuvut/PositiivisetLuvut.java
@@ -1,22 +1,5 @@
-/*
- *
- * Tehtävä:
- * --------------------
- * Toteuta tehtäväpohjaan luokkametodi 
- * public static List<Integer> positiiviset(List<Integer> luvut), 
- *
- * joka saa parametrinaan lukulistan ja jonka tulee palauttaa uusi 
- * lukulista, joka sisältää parametrina saadun listan sisältämät 
- * positiiviset luvut. 
- *
- * Toteuta metodi virtaa hyödyntäen! 
- * Kokeile lukujen keräämisen 
- * Collectors.toCollection(ArrayList::new) 
- * lisäksi komentoa Collectors.toList().
- *
- *
- */
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -25,8 +8,20 @@ public class PositiivisetLuvut {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        List<Integer> lista = new ArrayList<>();
         // testaa metodia täällä
 
+    }
+    
+    public static List<Integer> positiiviset(List<Integer> luvut) {
+        // saa parametrinaan lukulistan ja jonka tulee palauttaa 
+        // uusi lukulista, joka sisältää parametrina saadun listan 
+        // sisältämät positiiviset luvut.
+        List<Integer> posit = luvut.stream()
+                .filter(luku -> luku > 0)
+                .map(luku -> luku)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return posit;
     }
 
 }
