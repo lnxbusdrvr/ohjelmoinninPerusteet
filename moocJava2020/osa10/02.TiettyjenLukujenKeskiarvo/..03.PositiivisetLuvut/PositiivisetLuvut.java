05566c360f4888bb51cb45e5b2ae55e4e8b2cdac
Erkki Pokkinen <anticop@iki.fi>
2020-09-15 17:01:52 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/02.TiettyjenLukujenKeskiarvo/..03.PositiivisetLuvut/PositiivisetLuvut.java
@@ -0,0 +1,32 @@
+/*
+ *
+ * Tehtävä:
+ * --------------------
+ * Toteuta tehtäväpohjaan luokkametodi 
+ * public static List<Integer> positiiviset(List<Integer> luvut), 
+ *
+ * joka saa parametrinaan lukulistan ja jonka tulee palauttaa uusi 
+ * lukulista, joka sisältää parametrina saadun listan sisältämät 
+ * positiiviset luvut. 
+ *
+ * Toteuta metodi virtaa hyödyntäen! 
+ * Kokeile lukujen keräämisen 
+ * Collectors.toCollection(ArrayList::new) 
+ * lisäksi komentoa Collectors.toList().
+ *
+ *
+ */
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class PositiivisetLuvut {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // testaa metodia täällä
+
+    }
+
+}
