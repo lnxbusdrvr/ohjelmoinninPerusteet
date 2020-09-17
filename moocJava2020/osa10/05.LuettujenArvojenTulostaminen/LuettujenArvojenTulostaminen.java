fb2c73703f647871d98a4ccfb8d97e0cbda59767
Erkki Pokkinen <anticop@iki.fi>
2020-09-17 09:44:35 +0300

    Koodiaika 3min 57sek

--- moocJava2020/osa10/05.LuettujenArvojenTulostaminen/LuettujenArvojenTulostaminen.java
+++ moocJava2020/osa10/05.LuettujenArvojenTulostaminen/LuettujenArvojenTulostaminen.java
@@ -19,12 +19,26 @@
  */
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LuettujenArvojenTulostaminen {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+        
+        while(true) {
+            String syote = lukija.nextLine();
+            if(syote.equals("")) {
+                break;
+            }
+            lista.add(syote);
+        }
+        
+        String sanat = lista.stream()
+                .reduce("", (t, bo) -> t+bo+"\n");
+        System.out.println(sanat);
 
     }
 }
