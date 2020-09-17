1590c029e26d6e825478cd0f35afc3c96921b271
Erkki Pokkinen <anticop@iki.fi>
2020-09-17 09:49:58 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/06.RajatutLuvut/RajatutLuvut.java
@@ -0,0 +1,21 @@
+/*
+ *
+ * Tehtävä: 06.RajatutLuvut
+ * ------------------------
+ * Kirjoita ohjelma, joka lukee käyttäjältä lukuja. 
+ * Kun käyttäjä syöttää negatiivisen luvun, 
+ * lukeminen lopetetaan. Tulosta tämän jälkeen ne luvut, 
+ * jotka ovat välillä 1-5.
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RajatutLuvut {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
