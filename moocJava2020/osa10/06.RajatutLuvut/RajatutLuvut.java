3cdf6a77dcd66ccbe6686f10691f85f657809214
Erkki Pokkinen <anticop@iki.fi>
2020-09-17 10:33:20 +0300

    Koodiaika 8min 38sek

--- moocJava2020/osa10/06.RajatutLuvut/RajatutLuvut.java
+++ moocJava2020/osa10/06.RajatutLuvut/RajatutLuvut.java
@@ -10,12 +10,27 @@
  */
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RajatutLuvut {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        List<Integer> numerot = new ArrayList<>();
+        
+        while(true) {
+            int syote = Integer.valueOf(lukija.nextLine());
+            
+            if(syote < 0) {
+                break;
+            }
+            numerot.add(syote);
+        }
+        
+        numerot.stream()
+                .filter(n -> n >= 1 && n <= 5)
+                .forEach(n -> System.out.println(n));
 
     }
 }
