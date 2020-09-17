714a908849551890effbfa87ced771ef97dfae85
Erkki Pokkinen <anticop@iki.fi>
2020-09-17 09:30:12 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/05.LuettujenArvojenTulostaminen/LuettujenArvojenTulostaminen.java
@@ -0,0 +1,30 @@
+/*
+ *
+ * Tehtävä: 05.LuettujenArvojenTulostaminen
+ * ----------------------------------------
+ * Kirjoita ohjelma, joka lukee käyttäjältä merkkijonoja. 
+ * Lukeminen tulee lopettaa kun käyttäjä syöttää tyhjän merkkijonon. 
+ * Tulosta tämän jälkeen käyttäjän syöttämät merkkijonot.
+ *
+ * Esimerkkitulostus
+ *
+ * eka
+ * toka
+ * kolmas
+ *
+ * eka
+ * toka
+ * kolmas
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LuettujenArvojenTulostaminen {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+
+    }
+}
