c57dbf091bb7fad95c43ce8761753bc968e69767
Erkki Pokkinen <anticop@iki.fi>
2020-09-18 10:04:43 +0300

    Tehtävä ja valmis pohja

---
+++ moocJava2020/osa10/09.TiedostonRivit/TiedostonRivit.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * Tehtävä: 09.TiedostonRivit
+ * --------------------------
+ * Toteuta tehtäväpohjaan staattinen metodi 
+ * public static List<String> lue(String tiedosto), 
+ *
+ * joka lukee parametrina annetun merkkijonon nimisestä tiedostosta 
+ * rivit ja palauttaa ne merkkijonolistana.
+ *
+ */
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class TiedostonRivit {
+
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // testaa metodia täällä
+
+    }
+
+}
