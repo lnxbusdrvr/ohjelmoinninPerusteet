291337f0c0aff97229d61782fa5fc930382c7c69
Erkki Pokkinen <anticop@iki.fi>
2020-09-22 13:25:36 +0300

    Osa 2/3 Koodausaika 5min 50sek

--- moocJava2020/osa10/15.SaannollisetLausekkeet/Paaohjelma.java
+++ moocJava2020/osa10/15.SaannollisetLausekkeet/Paaohjelma.java
@@ -1,9 +1,23 @@
 
+import java.util.Scanner;
+
+
 
 public class Paaohjelma {
 
     public static void main(String[] args) {
         // tee tänne testikoodia josta kutsut ohjelmoitavia metodeja
+        Scanner lukija = new Scanner(System.in);
+
         
+
+        Tarkistin tarkistin = new Tarkistin();
+        System.out.println("Anna merkkijono:");
+        String merkkijono = lukija.nextLine();
+        
+        System.out.println(tarkistin.kaikkiVokaaleja(merkkijono));
+        //tarkistin.onViikonpaiva(merkkijono);
+        //tarkistin.kaikkiVokaaleja(merkkijono);
+        //tarkistin.kellonaika(merkkijono);
     }
 }
--- moocJava2020/osa10/15.SaannollisetLausekkeet/Tarkistin.java
+++ moocJava2020/osa10/15.SaannollisetLausekkeet/Tarkistin.java
@@ -5,5 +5,15 @@ public class Tarkistin {
     public boolean onViikonpaiva(String merkkijono) {
         return merkkijono.matches("ma|ti|ke|to|pe|la|su");
     }
+    
+    public boolean kaikkiVokaaleja(String merkkijono) {
+        return merkkijono.matches("[aeiouöäå]*");
+        /*if(merkkijono.matches("[aeiouyöäå]*")) {
+            System.out.println("true");
+            return true;
+        }
+        System.out.println("false");
+        return false;*/
+    }
 
 }
