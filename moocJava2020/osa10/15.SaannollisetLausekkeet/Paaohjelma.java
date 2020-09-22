e742d78fcde3eece4c242aa43503dce839ea1eed
Erkki Pokkinen <anticop@iki.fi>
2020-09-22 12:09:27 +0300

    Osa 1/3 Koodausaika 2min 25sek

--- moocJava2020/osa10/15.SaannollisetLausekkeet/Paaohjelma.java
+++ moocJava2020/osa10/15.SaannollisetLausekkeet/Paaohjelma.java
@@ -3,6 +3,7 @@
 public class Paaohjelma {
 
     public static void main(String[] args) {
-        // tee tänne testikoodia josta kutsut ohjelmoitavia metodeja    
+        // tee tänne testikoodia josta kutsut ohjelmoitavia metodeja
+        
     }
 }
--- moocJava2020/osa10/15.SaannollisetLausekkeet/Tarkistin.java
+++ moocJava2020/osa10/15.SaannollisetLausekkeet/Tarkistin.java
@@ -1,5 +1,9 @@
 
 
 public class Tarkistin {
+    
+    public boolean onViikonpaiva(String merkkijono) {
+        return merkkijono.matches("ma|ti|ke|to|pe|la|su");
+    }
 
 }
