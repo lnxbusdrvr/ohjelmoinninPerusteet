f635cb6c654122c3a34e2e50ab8353fbf1db68a7
Erkki Pokkinen <anticop@iki.fi>
2020-09-23 17:24:51 +0300

    Osa 1/4 Koodausaika 1min 20sek

---
+++ moocJava2020/osa10/16.EnumJaIteraattori/Koulutus.java
@@ -0,0 +1,21 @@
+
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public enum Koulutus {
+    FT("tohtori"), 
+    FM("maisteri"), 
+    LuK("kandidaatti"), 
+    FilYO("ylioppilas");
+    
+    private String arvo;
+
+    private Koulutus(String arvo) {
+        this.arvo = arvo;
+    }
+    
+    
+    
+}
