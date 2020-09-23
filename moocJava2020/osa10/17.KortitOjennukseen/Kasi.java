90e739be077e20c320d998832b9177bdc929d037
Erkki Pokkinen <anticop@iki.fi>
2020-09-23 20:30:23 +0300

    Osa 3/6 Koodausaika 1min 7sek

--- moocJava2020/osa10/17.KortitOjennukseen/Kasi.java
+++ moocJava2020/osa10/17.KortitOjennukseen/Kasi.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -23,6 +24,11 @@ public class Kasi {
         // tulostaa kädessä olevat kortit 
         // alla olevan esimerkin tyylillä
         this.kasi.stream().forEach(k -> System.out.println(k));
+    }
+    
+    public void jarjesta() {
+        // risti[0], ruutu[1], hertta[2], pata[3]
+        Collections.sort(kasi);
     }
     
 }
