d5bb668b6ddfb6b51c34d5b96759e8038dba7b98
Erkki Pokkinen <anticop@iki.fi>
2020-09-24 14:55:14 +0300

    Koodausaika 51sek

---
+++ moocJava2020/osa11/01.Asiakas/Asiakas.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Asiakas {
+    private String nimi;
+    private String osoite;
+    private String email;
+
+    public Asiakas(String nimi, String osoite, String email) {
+        this.nimi = nimi;
+        this.osoite = osoite;
+        this.email = email;
+    }           
+    
+}
