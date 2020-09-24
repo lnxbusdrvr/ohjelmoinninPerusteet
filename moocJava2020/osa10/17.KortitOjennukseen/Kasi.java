49bdd772a2b078bea75a4181897fa9eeb3a17aeb
Erkki Pokkinen <anticop@iki.fi>
2020-09-24 10:41:22 +0300

    Osa 4/6 Koodausaika 20min 44sek

--- moocJava2020/osa10/17.KortitOjennukseen/Kasi.java
+++ moocJava2020/osa10/17.KortitOjennukseen/Kasi.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -8,7 +9,7 @@ import java.util.List;
  *
  * @author lnxbusdrvr
  */
-public class Kasi {
+public class Kasi implements Comparable<Kasi> {
     private List<Kortti> kasi;
 
     public Kasi() {
@@ -29,6 +30,19 @@ public class Kasi {
     public void jarjesta() {
         // risti[0], ruutu[1], hertta[2], pata[3]
         Collections.sort(kasi);
+    }
+    
+    public int summa() {        
+        int summa = 0;
+        summa = this.kasi.stream()
+                .map(Kortti::getArvo)
+                .reduce(summa, Integer::sum);
+        return summa;
+    }
+
+    @Override
+    public int compareTo(Kasi t) {
+        return this.summa()-t.summa();
     }
     
 }
--- moocJava2020/osa10/17.KortitOjennukseen/Paaohjelma.java
+++ moocJava2020/osa10/17.KortitOjennukseen/Paaohjelma.java
@@ -5,7 +5,7 @@ public class Paaohjelma {
     public static void main(String[] args) {
         // tee tänne testikoodia
         
-        // Osa 2
+        /* Osa 2
         Kasi kasi = new Kasi();
 
         kasi.lisaa(new Kortti(2, Maa.RUUTU));
@@ -13,7 +13,35 @@ public class Paaohjelma {
         kasi.lisaa(new Kortti(12, Maa.HERTTA));
         kasi.lisaa(new Kortti(2, Maa.PATA));
 
-        kasi.tulosta();
+        kasi.tulosta();*/
+        
+        // Osa 4
+        Kasi kasi1 = new Kasi();
+
+        kasi1.lisaa(new Kortti(2, Maa.RUUTU));
+        kasi1.lisaa(new Kortti(14, Maa.PATA));
+        kasi1.lisaa(new Kortti(12, Maa.HERTTA));
+        kasi1.lisaa(new Kortti(2, Maa.PATA));
+
+        Kasi kasi2 = new Kasi();
+
+        kasi2.lisaa(new Kortti(14, Maa.RUUTU));
+        kasi2.lisaa(new Kortti(14, Maa.PATA));
+        kasi2.lisaa(new Kortti(11, Maa.HERTTA));
+        
+
+        int vertailu = kasi1.compareTo(kasi2);
+        System.out.println(vertailu);
+
+        if (vertailu < 0) {
+            System.out.println("arvokkaampi käsi sisältää kortit2");
+            kasi2.tulosta();
+        } else if (vertailu > 0){
+            System.out.println("arvokkaampi käsi sisältää kortit1");
+            kasi1.tulosta();
+        } else {
+            System.out.println("kädet yhtä arvokkaat");
+        }
 
         /*Kortti eka = new Kortti(2, Maa.RUUTU);
         Kortti toka = new Kortti(14, Maa.PATA);
