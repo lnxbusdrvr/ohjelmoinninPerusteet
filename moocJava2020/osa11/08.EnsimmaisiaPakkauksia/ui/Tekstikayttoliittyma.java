79af817d20eca18bf265784fa9ce9f5d4e04a9ab
Erkki Pokkinen <anticop@iki.fi>
2020-09-29 10:58:51 +0300

    Osa 2/3 Koodausaika 27sek

---
+++ moocJava2020/osa11/08.EnsimmaisiaPakkauksia/ui/Tekstikayttoliittyma.java
@@ -0,0 +1,16 @@
+
+package mooc.ui;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Tekstikayttoliittyma implements Kayttoliittyma {
+
+    @Override
+    public void paivita() {
+        System.out.println("Päivitetään käyttöliittymää");        
+    }
+    
+    
+}
