fffa5e276992846bced5e7e1fa4834eac79f7633
Erkki Pokkinen <anticop@iki.fi>
2020-09-29 11:16:30 +0300

    Osa 3/3 Koodausaika 1min 49sek

---
+++ moocJava2020/osa11/08.EnsimmaisiaPakkauksia/logiikka/Sovelluslogiikka.java
@@ -0,0 +1,24 @@
+
+package mooc.logiikka;
+import mooc.ui.Kayttoliittyma;
+
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Sovelluslogiikka {
+    private Kayttoliittyma kayttoliittyma;
+
+    public Sovelluslogiikka(Kayttoliittyma kayttoliittyma) {
+        this.kayttoliittyma = kayttoliittyma;
+    }
+    
+    public void suorita(int montaKertaa) {
+        for(int i = 0 ; i < montaKertaa; i++) {
+            System.out.println("Sovelluslogiikka toimii");
+            this.kayttoliittyma.paivita();
+        }
+    }
+    
+}
