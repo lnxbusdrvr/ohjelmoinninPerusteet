ed42d2bcf5f7086f91dd038f3bc63d28afd866a6
Erkki Pokkinen <anticop@iki.fi>
2020-10-01 11:15:00 +0300

    Koodausaika 3min 19sek

--- moocJava2020/osa11/10.Lentokentta/lentokentta/Main.java
+++ moocJava2020/osa11/10.Lentokentta/lentokentta/Main.java
@@ -1,9 +1,19 @@
 package lentokentta;
 
+import java.util.Scanner;
+import lentokentta.logiikka.Lentohallinta;
+import lentokentta.ui.Tekstikayttoliittyma;
 
 public class Main {
 
     public static void main(String[] args) {
         // Kirjoita pääohjelma tänne. Omien luokkien tekeminen on hyödyllistä.
+        Scanner lukija = new Scanner(System.in);
+        Lentohallinta lh = new Lentohallinta();
+        Tekstikayttoliittyma ui = new Tekstikayttoliittyma(lh, lukija);
+        
+        ui.kaynnista();
+        
+        
     }
 }
