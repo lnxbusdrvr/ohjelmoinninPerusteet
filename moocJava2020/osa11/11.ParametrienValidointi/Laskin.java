89efa092ab356c0615862d054f3d45e0bdbbd7f2
Erkki Pokkinen <anticop@iki.fi>
2020-10-02 10:56:10 +0300

    Osa 2/2 Koodausaika 6min 18sek

--- moocJava2020/osa11/11.ParametrienValidointi/Laskin.java
+++ moocJava2020/osa11/11.ParametrienValidointi/Laskin.java
@@ -3,19 +3,30 @@ package validointi;
 public class Laskin {
 
     public int kertoma(int luvusta) {
-
         int kertoma = 1;
-        for (int i = 1; i <= luvusta; i++) {
-            kertoma *= i;
+        if(luvusta >= 0) {
+            for (int i = 1; i <= luvusta; i++) {
+                kertoma *= i;
+            }
+        }else{
+            throw new IllegalArgumentException("Ei negatiivisia lukuja");
         }
 
         return kertoma;
     }
 
     public int binomikerroin(int joukonKoko, int osaJoukonKoko) {
-
-        int osoittaja = kertoma(joukonKoko);
-        int nimittaja = kertoma(osaJoukonKoko) * kertoma(joukonKoko - osaJoukonKoko);
+        int osoittaja = 0;
+        int nimittaja = 0;
+        if(joukonKoko >= 0 && osaJoukonKoko >= 0) {
+            if(osaJoukonKoko > joukonKoko) {
+                osoittaja = kertoma(joukonKoko);
+                nimittaja = kertoma(osaJoukonKoko) * 
+                        kertoma(joukonKoko - osaJoukonKoko);
+            }
+        }else{
+            throw new IllegalArgumentException("Ei negatiivisia lukuja");
+        }
 
         return osoittaja / nimittaja;
     }
