4b64d31a42a66f13797c5751a4cc188585b4a7e3
Erkki Pokkinen <anticop@iki.fi>
2020-10-05 22:19:30 +0300

    Osa 3/4 Koodausaika 7min 45sek

--- moocJava2020/osa11/13.MuistavaSanakirja/MuistavaSanakirja.java
+++ moocJava2020/osa11/13.MuistavaSanakirja/MuistavaSanakirja.java
@@ -1,6 +1,8 @@
 package sanakirja;
 
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  *
@@ -8,11 +10,20 @@ import java.util.HashMap;
  */
 public class MuistavaSanakirja {
     private HashMap<String, String> sanakirja;
+    private HashMap<String, String> sanakirjaTiedostosta;
+    private String tiedosto;
 
     public MuistavaSanakirja() {
         this.sanakirja = new HashMap<>();
+        this.sanakirjaTiedostosta = new HashMap<>();
     }
-    
+
+    public MuistavaSanakirja(String tiedosto) {
+        this.sanakirja = new HashMap<>();
+        this.sanakirjaTiedostosta = new HashMap<>();
+        this.tiedosto = tiedosto;
+    }    
+        
     public void lisaa(String sana, String kaannos) {
         // Ensi kerralla lue paremmin ohjeet
         // kun tämä metodi oli vääriin niin tähään kului taas tunt(e)i aikaa
@@ -44,6 +55,26 @@ public class MuistavaSanakirja {
             this.sanakirja.remove(this.sanakirja.get(sana));
         }if(this.sanakirja.containsKey(sana)) {
             this.sanakirja.remove(sana);
+        }
+    }
+    
+    public boolean lataa() {
+        try{
+            Scanner tiedostonLukija = new Scanner(Paths.get(this.tiedosto));
+            
+            while (tiedostonLukija.hasNextLine()) {
+                String rivi = tiedostonLukija.nextLine();
+                // pilkotaan rivi :-merkkien kohdalta
+                String[] osat = rivi.split(":");
+                
+                //Lisää listalle
+                this.lisaa(osat[0], osat[1]);                
+            }
+            return true;
+        }catch (Exception e) {
+            return false;
+        }finally{
+            
         }
     }
     
