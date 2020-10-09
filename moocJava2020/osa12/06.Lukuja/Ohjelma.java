2cefe2b429a68755dd8d6754b68c3247bedec7be
Erkki Pokkinen <anticop@iki.fi>
2020-10-09 20:30:52 +0300

    Koodausaika 5min 18sek

--- moocJava2020/osa12/06.Lukuja/Ohjelma.java
+++ moocJava2020/osa12/06.Lukuja/Ohjelma.java
@@ -6,6 +6,18 @@ public class Ohjelma {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+
+        System.out.println("Montako satunnaislukua tulostetaan?");
+        int maara = Integer.valueOf(lukija.nextLine());
+
+        for(int i = 0; i < maara;i++) {
+          System.out.println(heita());
+        }
+    }
+
+    public static int heita() {
+        Random satu = new Random();
+        return satu.nextInt(11);
     }
 
 }
