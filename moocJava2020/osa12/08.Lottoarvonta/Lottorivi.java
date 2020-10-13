5e2585ddb4538f519d7a9f9d6be972f298d22d77
Erkki Pokkinen <anticop@iki.fi>
2020-10-13 16:09:43 +0300

    Koodausaika 40min 5sek

--- moocJava2020/osa12/08.Lottoarvonta/Lottorivi.java
+++ moocJava2020/osa12/08.Lottoarvonta/Lottorivi.java
@@ -1,12 +1,16 @@
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Lottorivi {
 
     private ArrayList<Integer> numerot;
+    private Random rndm;
 
     public Lottorivi() {
+        //Ei auta:
+        //this.numerot = new ArrayList<>();
         // Arvo numerot heti LottoRivin luomisen yhteydessä
         this.arvoNumerot();
     }
@@ -17,13 +21,46 @@ public class Lottorivi {
 
     public void arvoNumerot() {
         // Alustetaan lista numeroille
-        this.numerot = new ArrayList<>();
+        // Numeroiden alustus virran sisällä
+        //this.numerot = new ArrayList<>();
         // Kirjoita numeroiden arvonta tänne
         // kannattanee hyödyntää metodia sisaltaaNumeron
+        this.rndm = new Random();
+        //                       ints(mista, mihin)
+        this.numerot = this.rndm.ints(1, 41)
+                .distinct()
+                .limit(7)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     public boolean sisaltaaNumeron(int numero) {
         // Testaa tässä onko numero jo arvottujen numeroiden joukossa
+        
+        //for(int nrot : this.numerot) {
+        for(int i = 0; i < this.numerot.size(); i++) {
+            if(this.numerot.get(i) == numero) {
+            // Debug:
+            //System.out.println("this.numero: "+this.numerot.get(i)
+            //+", vertailu_numero: "+numero+"   koko: "
+            //+this.numerot.size()+" onko totta: "
+            //+(this.numerot.get(i) == numero));
+                return true;
+            }
+        }
         return false;
+    }
+    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int nro: this.numerot) {
+            sb.append(nro+", ");
+        }
+        return "Lottonumerot: \n"+sb; 
     }
 }
--- moocJava2020/osa12/08.Lottoarvonta/Ohjelma.java
+++ moocJava2020/osa12/08.Lottoarvonta/Ohjelma.java
@@ -6,11 +6,14 @@ public class Ohjelma {
     public static void main(String[] args) {
         Lottorivi rivi = new Lottorivi();
         ArrayList<Integer> lottonumerot = rivi.numerot();
+        
+        rivi.sisaltaaNumeron(29);
+        System.out.println(rivi);
 
-        System.out.println("Lottonumerot:");
+        /*System.out.println("Lottonumerot:");
         for (Integer numero : lottonumerot) {
             System.out.print(numero + " ");
         }
-        System.out.println("");
+        System.out.println("");*/
     }
 }
