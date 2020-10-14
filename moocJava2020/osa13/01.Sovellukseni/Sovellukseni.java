bf4d1c2488c0e2f765a39e77a233e292d6cb6a64
Erkki Pokkinen <anticop@iki.fi>
2020-10-14 22:16:02 +0300

    Koodausaika 4min 31sek

--- moocJava2020/osa13/01.Sovellukseni/Sovellukseni.java
+++ moocJava2020/osa13/01.Sovellukseni/Sovellukseni.java
@@ -1,11 +1,20 @@
 package sovellukseni;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Sovellukseni {
-    
+public class Sovellukseni extends Application {    
 
     public static void main(String[] args) {
+        launch(Sovellukseni.class);
         System.out.println("Hello world!");
+    }
+    
+    @Override
+    public void start(Stage ikkuna) throws Exception {
+        ikkuna.setTitle("Sovellukseni");
+        ikkuna.show();
+        
     }
 
 }
