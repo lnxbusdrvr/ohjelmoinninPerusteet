80d9a93745c1105ca8b134b4515a4824185e560c
Erkki Pokkinen <anticop@iki.fi>
2020-10-14 20:57:50 +0300

    Osa 4/4 Koodausaika 14min 22sek, huh

--- moocJava2020/osa12/10.Taikanelio/Taikaneliotehdas.java
+++ moocJava2020/osa12/10.Taikanelio/Taikaneliotehdas.java
@@ -1,11 +1,67 @@
+/*
+ * Algoritmi:
+ * 
+ * eka = (taulunKoko/SarakkeenKoko -1)/2
+ * toka = ylos ja oikealle
+ *      jos seinä tulee vastaan korkeussuunnassa/pituussuunassa, 
+ *      siirytään vastkkaisen puolen alkuun
+ * yksi ylös ja oikealle
+ *      jos seinä tulee vastaan leveyssuunassa, 
+ *      siirytään vastkkaisen puolen alkuun
+ * Jos kohdassa onkin luku, siirytään askel eteenpäin, 
+ * siitä askeleesta mistä viimeksi aloitettiin.
+ * 
+ */
 
 public class Taikaneliotehdas {
 
     public Taikanelio luoTaikanelio(int koko) {
 
         Taikanelio nelio = new Taikanelio(koko);
-
         // toteuta taikaneliön luominen Siamese method -algoritmilla tänne
+        int nro = 1;
+        int rivi = 0;
+        int sarake = ((koko-1) / 2);
+        int nykyRivi = 0;
+        int nykySarake = ((koko-1) / 2);
+        for(int i = 0; i < (koko * koko); i++) {
+            nelio.asetaArvo(sarake, rivi, nro);    // Asettaa arvon
+            // Muutetaan sijaintia
+            
+            //Siirrä ylös
+            rivi -= 1;
+            //Jos pituussuunassa yli laidan
+            if(rivi < 0) {
+                // Siirrä vastakkaisen puolen alkuun
+                rivi = (koko-1);
+            }
+            // Siirrä oikealle
+            sarake += 1;
+            //Jos leveyssuunassa yli laidan
+            if(sarake > (koko-1)) {
+                // Siirrä vastakkaisen puolen alkuun
+                sarake = 0;
+            }
+            // Kun neliössä on jo numero
+            if(nelio.annaArvo(sarake, rivi) > 0) {
+                // Siirrytään kohdan alusta
+                rivi = nykyRivi;
+                sarake = nykySarake;
+                // Askel oikealle
+                rivi += 1;
+                // Jos mentiin leveyssuunassa yli laidan
+                if(rivi < 0) {
+                    // Mennän toisenpuolen alkuun/loppuun
+                    rivi = (koko-1);
+                }
+            }
+            // Sijoitetaan wanhempien muuttuijen arvot
+            // Seuravalle kerralle
+            nykyRivi = rivi;
+            nykySarake = sarake;
+            //Kasvatetaan numeroa ja aloitetaan alusta
+            nro++;            
+        }
         return nelio;
     }
 
