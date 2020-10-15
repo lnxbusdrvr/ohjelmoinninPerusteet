0e226766c2aaf9d92434faa296a338cc8e4f83b7
Erkki Pokkinen <anticop@iki.fi>
2020-10-15 20:55:38 +0300

    Koodausaika 8min 55sek

--- moocJava2020/osa13/02.NappiJaTekstielementti/NappiJaTekstielementtiSovellus.java
+++ moocJava2020/osa13/02.NappiJaTekstielementti/NappiJaTekstielementtiSovellus.java
@@ -1,11 +1,37 @@
 package nappijatekstielementti;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class NappiJaTekstielementtiSovellus {
+
+public class NappiJaTekstielementtiSovellus extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Kayttoliittyma kl = new Kayttoliittyma();
+        kl.aloita();
+    }
+    
+    @Override
+    public void start(Stage ikkuna) throws Exception {
+        Button nappi = new Button("Nappi");
+        Label teksti = new Label();
+        
+        teksti.setText("Hei Maailma!");
+        
+        FlowPane komponentit = new FlowPane();
+        komponentit.getChildren().add(nappi);
+        komponentit.getChildren().add(teksti);
+        
+        Scene nakyma = new Scene(komponentit);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
     }
 
 }
