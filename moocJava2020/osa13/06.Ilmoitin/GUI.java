00fed3ae4d2a1c93b7314f87a856a8e205815650
Erkki Pokkinen <anticop@iki.fi>
2020-10-18 19:40:24 +0300

    Olio-versio

---
+++ moocJava2020/osa13/06.Ilmoitin/GUI.java
@@ -0,0 +1,52 @@
+package ilmoitin;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+// ei-stream versiolle:
+//import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage ikkuna) throws Exception {
+        Button nappi = new Button("Päivitä");
+        TextField ylateksti = new TextField();
+        Label alateksti = new Label();
+        
+        /* ei-stream versio
+        nappi.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent tapahtuma) {
+                alateksti.setText(ylateksti.getText());
+            }
+        });
+        */
+        
+        nappi.setOnAction((ActionEvent tapahtuma) -> {
+            alateksti.setText(ylateksti.getText());
+        });
+        
+        VBox asetelma = new VBox();
+        asetelma.getChildren().addAll(ylateksti, nappi, alateksti);
+        
+        Scene nakyma = new Scene(asetelma);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+    }
+    
+    public void aloita() {
+        launch(GUI.class);
+    }
+    
+}
---
+++ moocJava2020/osa13/06.Ilmoitin/OlioMain.java
@@ -0,0 +1,13 @@
+package ilmoitin;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class OlioMain {
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.aloita();
+    }
+    
+}
