b461586e389b1b7d0a5db734d75ba8e9da57415c
Erkki Pokkinen <anticop@iki.fi>
2020-10-20 18:50:28 +0300

    Koodausaika 16min 16sek

--- moocJava2020/osa13/09.UseampiNakyma/UseampiNakyma.java
+++ moocJava2020/osa13/09.UseampiNakyma/UseampiNakyma.java
@@ -1,9 +1,65 @@
 package sovellus;
 
-public class UseampiNakyma {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UseampiNakyma extends Application{
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ohjelma käynnistyy!");
+        Application.launch(GUI.class);
+    }
+    
+    @Override
+    public void start(Stage ikkuna) {
+        // ekaNakyma
+        BorderPane ekaAsetelma = new BorderPane();
+        
+        ekaAsetelma.setTop(new Label("Eka näkymä!"));
+        Button tokaan = new Button("Tokaan näkymään!");
+        ekaAsetelma.setCenter(tokaan);
+        
+        // tokaNakyma      
+        VBox tokaAsetelma = new VBox();
+        
+        Button kolmanteen = new Button("Kolmanteen näkymään!");
+        
+        tokaAsetelma.getChildren().add(kolmanteen);
+        tokaAsetelma.getChildren().add(new Label("Toka näkymä!"));
+        
+        // kolmasNakyma
+        GridPane kolmasAsetelma = new GridPane();
+        
+        kolmasAsetelma.add(new Label("Kolmas näkymä!"), 0, 0);
+        Button ekaan = new Button("Ekaan näkymään!");
+        kolmasAsetelma.add(ekaan, 1, 1);
+        
+        Scene ekaNakyma = new Scene(ekaAsetelma);
+        Scene tokaNakyma = new Scene(tokaAsetelma);
+        Scene kolmasNakyma = new Scene(kolmasAsetelma);
+        
+        tokaan.setOnAction((event) -> {
+            ikkuna.setScene(tokaNakyma);
+        });
+        
+        kolmanteen.setOnAction((event) -> {
+            ikkuna.setScene(kolmasNakyma);
+        });
+        
+        ekaan.setOnAction((event) -> {
+            ikkuna.setScene(ekaNakyma);
+        });
+        
+        // Ikkunaan määritellään vain ekaNakyma
+        ikkuna.setScene(ekaNakyma);
+        ikkuna.show();       
+        
     }
 
 }
