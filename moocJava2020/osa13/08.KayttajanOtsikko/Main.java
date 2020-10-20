31b18b8b6306b51291e79701ffdc03c6a4d0c216
Erkki Pokkinen <anticop@iki.fi>
2020-10-20 16:47:09 +0300

    Koodausaika 9min 15sek

--- moocJava2020/osa13/08.KayttajanOtsikko/Main.java
+++ moocJava2020/osa13/08.KayttajanOtsikko/Main.java
@@ -1,10 +1,20 @@
 package otsikko;
 
+import java.util.Scanner;
+import javafx.application.Application;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner lukija = new Scanner(System.in);
+        
+        System.out.print("Syötä otsikko: ");
+        String syote = lukija.nextLine();
+        
+        System.out.println("Ohjelma alkaa");
+        Application.launch(KayttajanOtsikko.class,
+                "--ots="+syote);
 
     }
 
