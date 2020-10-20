6fdb0addcd789c32cbba7c8288bf92a11c77472a
Erkki Pokkinen <anticop@iki.fi>
2020-10-20 13:50:33 +0300

    Olio-versio

---
+++ moocJava2020/osa13/07.TekstitilastointiaOsa2/Kayttoliittyma.java
@@ -0,0 +1,68 @@
+package tekstitilastointia;
+
+import java.util.Arrays;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class Kayttoliittyma extends Application {
+
+    @Override
+    public void start(Stage ikkuna) throws Exception {
+        BorderPane asetelma = new BorderPane();
+        
+        TextArea tekstia = new TextArea();
+        
+        HBox alarivi = new HBox();
+        
+        Label txtKirjaimia = new Label("Kirjaimia: ");
+        Label txtSanoja = new Label("Sanoja: ");
+        Label txtPisin = new Label("Pisin sana on: ");
+        
+        
+        
+        
+        tekstia.textProperty().addListener((muutos, vanhaArvo, uusiArvo) -> {
+          int kirjaimia = uusiArvo.length();
+          String[] palat = uusiArvo.split(" ");
+          int sanoja = palat.length;
+          String pisin = Arrays.stream(palat)
+                  .sorted((s1, s2) -> s2.length() - s1.length())
+                  //.findAny() // Ei vaikutusta
+                  .findFirst()
+                  .get();
+          
+          txtKirjaimia.setText("Kirjaimia: "+kirjaimia);
+          txtSanoja.setText("Sanoja: "+sanoja);
+          txtPisin.setText("Pisin sana on: "+pisin);                 
+          
+        });
+        
+        alarivi.setSpacing(10);
+        alarivi.getChildren().add(txtKirjaimia);
+        alarivi.getChildren().add(txtSanoja);
+        alarivi.getChildren().add(txtPisin);
+        
+        asetelma.setCenter(tekstia);
+        asetelma.setBottom(alarivi);
+        
+        Scene nakyma = new Scene(asetelma);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
+        
+    }
+    
+    public void aloita() {
+        launch(Kayttoliittyma.class);
+    }
+    
+}
---
+++ moocJava2020/osa13/07.TekstitilastointiaOsa2/KayttoliittymaMain.java
@@ -0,0 +1,14 @@
+package tekstitilastointia;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class KayttoliittymaMain {
+    
+    public static void main(String[] args) {
+        Kayttoliittyma kl = new Kayttoliittyma();
+        kl.aloita();
+    }
+    
+}
