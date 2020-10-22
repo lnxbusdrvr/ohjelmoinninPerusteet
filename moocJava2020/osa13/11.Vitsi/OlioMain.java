dd4f453e2d9fa3c2dfca2f74c454d4ce4a172645
Erkki Pokkinen <anticop@iki.fi>
2020-10-22 12:21:53 +0300

    Olio-versio

---
+++ moocJava2020/osa13/11.Vitsi/OlioMain.java
@@ -0,0 +1,16 @@
+package sovellus;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author lnxbusdrvr
+ */
+public class OlioMain {
+    
+    public static void main(String[] args) {
+        System.out.println("Aloitetaan Olio!");
+        Application.launch(GUI.class);
+    }
+    
+}
