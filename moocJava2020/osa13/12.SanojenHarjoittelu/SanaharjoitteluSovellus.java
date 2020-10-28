6985a1273dcd0ab75abe62fb76df825cd3b4fa2f
Erkki Pokkinen <anticop@iki.fi>
2020-10-28 21:33:37 +0200

    Koodausaika 46sek

--- moocJava2020/osa13/12.SanojenHarjoittelu/SanaharjoitteluSovellus.java
+++ moocJava2020/osa13/12.SanojenHarjoittelu/SanaharjoitteluSovellus.java
@@ -1,10 +1,13 @@
 package sovellus;
 
+import javafx.application.Application;
+
 
 // END SOLUTION
 public class SanaharjoitteluSovellus {
 
 
     public static void main(String[] args) {
+        Application.launch(HarjoitteluSovellus.class);
     }
 }
