9139791d30ae7af71f9bbdb4cdd7d7f985cc6d87
Erkki Pokkinen <anticop@iki.fi>
2020-10-29 21:37:13 +0200

    Osa 1/3 Koodausaika 27min 43sek

--- moocJava2020/osa13/13.Ristinolla/RistinollaSovellus.java
+++ moocJava2020/osa13/13.Ristinolla/RistinollaSovellus.java
@@ -1,11 +1,62 @@
 package ristinolla;
 
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-public class RistinollaSovellus {
+public class RistinollaSovellus extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ohjelma alkaa!");
+        launch(RistinollaSovellus.class);
+    }
+    
+    @Override
+    public void start(Stage ikkuna)  {
+        BorderPane asettelu = new BorderPane();
+        
+        //HBox ylavalikko = new HBox();
+        
+        Label ylateksti = new Label("Vuoro: ");
+        //Label vuoro = new Label("X");
+        
+        ylateksti.setFont(Font.font("Monospaced", 40));
+        
+        //ylavalikko.getChildren().addAll(ylateksti, vuoro);
+        
+                
+        GridPane ristikko = new GridPane();
+        
+        // Asetetaan ristikon napit        
+        // Luodaan 3x3 ristikko
+        for (int rivi = 1; rivi <= 3; rivi++) {
+            for (int sarake = 1; sarake <= 3; sarake++) {
+                // Javassa ei ole mahdollista luoda muuttuijia, jonka nimessä
+                // oleva numero kasvaa, 
+                // nappi11, nappi12, nappi13
+                // nappi21, nappi22, nappi23 jne
+                ristikko.add(new Button(), rivi, sarake);
+            }
+        }
+        
+        ristikko.setHgap(10);
+        ristikko.setVgap(10);
+        ristikko.setAlignment(Pos.CENTER);
+        
+        asettelu.setTop(ylateksti);
+        asettelu.setCenter(ristikko);
+        
+        Scene nakyma = new Scene(asettelu, 350, 350);
+        
+        ikkuna.setScene(nakyma);
+        ikkuna.show();
     }
 
 }
