85b0b72cf1c1514b592fca3cd581096a09a878b0
Erkki Pokkinen <anticop@iki.fi>
2020-11-05 23:49:07 +0200

    Osa 3/3 Koodausaika 39min 27sek

--- moocJava2020/osa14/03.Saastolaskuri/SaastolaskuriSovellus.java
+++ moocJava2020/osa14/03.Saastolaskuri/SaastolaskuriSovellus.java
@@ -3,6 +3,7 @@ package sovellus;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -33,31 +34,35 @@ public class SaastolaskuriSovellus extends Application {
         //                         mistä, mihin, missäKohtaaOletuksena
         Slider kkliukuri = new Slider(25, 250, 25);
         //                           mistä, mihin, missäKohtaaOletuksena
-        Slider vuosiliukuri = new Slider(0, 10, 0);
+        //Slider korkoliukuri = new Slider(0, 10, 0);
+        Slider korkoliukuri = new Slider();
+        korkoliukuri.setMin(0);
+        korkoliukuri.setMax(10);
+        korkoliukuri.setMinorTickCount(10);
         
         // Näytä pystyviivat/putkimerkkit
         kkliukuri.setShowTickMarks(true);
-        vuosiliukuri.setShowTickMarks(true);
+        korkoliukuri.setShowTickMarks(true);
   
         // Näyttää arvot
         kkliukuri.setShowTickLabels(true);
-        vuosiliukuri.setShowTickLabels(true);
+        korkoliukuri.setShowTickLabels(true);
         
         // Otetan numerot pystyviivojen/putkimerkkien kohdalta
         kkliukuri.setSnapToTicks(true);
-        vuosiliukuri.setSnapToTicks(true);
+        //korkoliukuri.setSnapToTicks(true);
         
         // Asetetaan maximiarvo, testit toimii nopeammin ja vähemmän virheitä
         kkliukuri.setBlockIncrement(250);
-        vuosiliukuri.setBlockIncrement(10);
+        //korkoliukuri.setBlockIncrement(10);
         
         // Teksti ennen liukuria
         Label kkliukuriTxt = new Label("Kuukausittainen tallennus");
-        Label vuosiliukuriTxt = new Label("Vuosittainen korko");
+        Label korkoliukuriTxt = new Label("Vuosittainen korko");
         
         // Teksti liukurin jälkeen
         Label kkliukuriArvo = new Label(""+kkliukuri.getValue()); // Otetaan arvoo Sliderista
-        Label vuosilikuriArvo = new Label(""+vuosiliukuri.getValue()); // Otetaan arvoo Sliderista
+        Label korkoliukuriArvo = new Label(""+korkoliukuri.getValue()); // Otetaan arvoo Sliderista
         
         // kuukausikorkopalkki
         BorderPane kkpalkki = new BorderPane();
@@ -65,16 +70,20 @@ public class SaastolaskuriSovellus extends Application {
         kkpalkki.setLeft(kkliukuriTxt);
         kkpalkki.setCenter(kkliukuri);
         kkpalkki.setRight(kkliukuriArvo);
+        kkpalkki.setPadding(new Insets(10));  // Säädöt saa viivan oikeaksi
         
         // Vuosikorkosiivu
-        BorderPane vuosipalkki = new BorderPane();
+        BorderPane korkopalkki = new BorderPane();
         // Asetetaan komponentin kuukausipalkkiin
-        vuosipalkki.setLeft(vuosiliukuriTxt);
-        vuosipalkki.setCenter(vuosiliukuri);
-        vuosipalkki.setRight(vuosilikuriArvo);
+        korkopalkki.setLeft(korkoliukuriTxt);
+        korkopalkki.setCenter(korkoliukuri);
+        korkopalkki.setRight(korkoliukuriArvo);
+        korkopalkki.setPadding(new Insets(10)); // Säädöt saa viivan oikeaksi
        
         // Asetetaan liukurin pystysuuntaisen boxin sisään
-        ylalaita.getChildren().addAll(kkpalkki, vuosipalkki);
+        ylalaita.getChildren().addAll(kkpalkki, korkopalkki);
+        ylalaita.setPadding(new Insets(10)); // Säädöt saa viivan oikeaksi
+        ylalaita.setSpacing(10); // Säädöt saa viivan oikeaksi
                 
         // Muodostetaan akselit
         NumberAxis xAkseli = new NumberAxis(0, 30, 1);
@@ -84,32 +93,42 @@ public class SaastolaskuriSovellus extends Application {
         // Aloitetaan viivakaavio
         LineChart viivakaavio = new LineChart<>(xAkseli, yAkseli);
         viivakaavio.setTitle("Säästölaskuri");
+        viivakaavio.setAnimated(false); // Säädöt saa viivan oikeaksi
         viivakaavio.setLegendVisible(false);
         
-        // Luodaan kaavaa
+        // Luodaan kaavio
         XYChart.Series talletukset = new XYChart.Series<>();
-                
-        
+        XYChart.Series korkotall = new XYChart.Series<>();
         
         asettelu.setTop(ylalaita);
         asettelu.setCenter(viivakaavio);
         
         // Likurien toiminta
         kkliukuri.setOnMouseReleased(e -> {
-            kkliukuriArvo.setText("" + kkliukuri.getValue());
-            //saastot(kkliukuri.getValue(), korko.getValue(), talletukset, saastotKoroilla);
-            saastot(Double.valueOf(kkliukuriArvo.getText()), talletukset);
+            //                                    yhden desimalin tarkuudella
+            //kkliukuriArvo.setText(String.format("%.1f", kkliukuri.getValue()));
+            kkliukuriArvo.setText("" +kkliukuri.getValue());
+            saastot(kkliukuri.getValue(), korkoliukuri.getValue(), talletukset, korkotall);
         });
         
-        vuosiliukuri.setOnMouseReleased(e -> {
-            vuosilikuriArvo.setText("" + vuosiliukuri.getValue());
-            //saastot(kkliukuri.getValue(), korko.getValue(), talletukset, saastotKoroilla);
-            saastot(Double.valueOf(kkliukuriArvo.getText()), talletukset);
+        // Lisätään data viivastolle
+        viivakaavio.getData().add(talletukset);
+        
+        korkoliukuri.setOnMouseReleased(e -> {
+            //                                    kahden desimalin tarkuudella
+            //korkoliukuriArvo.setText(String.format("%.2f", korkoliukuri.getValue()));
+            //korkoliukuriArvo.setText("" +korkoliukuri.getValue());
+            String korkoString = "" + korkoliukuri.getValue();
+            if (korkoString.length() > 4) {
+                korkoString = korkoString.substring(0, 4);
+            }
+            korkoliukuriArvo.setText(korkoString);
+            saastot(kkliukuri.getValue(), korkoliukuri.getValue(), talletukset, korkotall);
         });
         // Toiminto loppuu
         
         // Lisätään data viivastolle
-        viivakaavio.getData().add(talletukset);
+        viivakaavio.getData().add(korkotall);
         
         // Luodaan näkymä
         Scene nakyma = new Scene(asettelu, 640, 470);
@@ -118,14 +137,23 @@ public class SaastolaskuriSovellus extends Application {
         ikkuna.show();
     }
     
-    public void saastot(double kkarvo, XYChart.Series kktalletus) {
+    private void saastot(double kksaasto, double korko, XYChart.Series kktalletus, XYChart.Series korkotalletus) 
+    {
         // Jotta vanhat arvot poistuisivat, tyhjennetään kaavio
         kktalletus.getData().clear();
-        // Eka kohta manuaalisti
-        //kktalletus.getData().add(new XYChart.Data(0, 0));
-        for(int i = 0; i <=30; i++) {
-            int saasto = i * (int)kkarvo * 12;
-            kktalletus.getData().add(new XYChart.Data(i, saasto));
+        korkotalletus.getData().clear();
+        // Luodaan nollakorko
+        kktalletus.getData().add(new XYChart.Data(0, 0));
+        korkotalletus.getData().add(new XYChart.Data(0, 0));
+        
+        double edellinen = 0;
+        
+        for(int i = 1; i <=30; i++) {
+            kktalletus.getData().add(new XYChart.Data(i, i * kksaasto * 12));
+            edellinen +=kksaasto * 12;
+            edellinen *= (1 + korko / 100.0);
+            
+            korkotalletus.getData().add(new XYChart.Data(i, edellinen));            
         }
     }
 
