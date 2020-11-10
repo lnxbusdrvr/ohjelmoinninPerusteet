862c7023bd99643905aa2fb5d041834f109c10ec
Erkki Pokkinen <anticop@iki.fi>
2020-11-10 14:39:31 +0200

    Koodausaika 3min 25sek

--- moocJava2020/osa14/08.Hurraa/HurraaSovellus.java
+++ moocJava2020/osa14/08.Hurraa/HurraaSovellus.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.media.AudioClip;
 
 public class HurraaSovellus extends Application {
 
@@ -13,8 +14,17 @@ public class HurraaSovellus extends Application {
         BorderPane pane = new BorderPane();
 
         Button nappi = new Button("Hurraa!");
-        pane.setCenter(nappi);
+        
 
+        // Äänitiedosto
+        AudioClip aanitiedosto = new AudioClip("file:front-desk-bells-daniel_simon.wav");
+        
+        // Napin toiminto
+        nappi.setOnMouseClicked(( tapahtuma) -> {
+            aanitiedosto.play();    // Ääni toimii ainakin kuulokkeilla
+        });
+        
+        pane.setCenter(nappi);
 
         Scene scene = new Scene(pane, 600, 400);
 
