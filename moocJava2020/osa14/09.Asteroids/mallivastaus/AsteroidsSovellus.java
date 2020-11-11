b82ffec86fc2ed72fb40cf89d5a6862a946ce29d
Erkki Pokkinen <anticop@iki.fi>
2020-11-11 23:34:40 +0200

    Mallivastaus

---
+++ moocJava2020/osa14/09.Asteroids/mallivastaus/AsteroidsSovellus.java
@@ -0,0 +1,16 @@
+package asteroids;
+ 
+public class AsteroidsSovellus {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hei maailma!");
+    }
+ 
+    public static int osiaToteutettu() {
+        // Ilmoita tämän metodin palautusarvolla kuinka monta osaa olet tehnyt
+        // malli tehtävän ratkaisemiseksi löytyy materiaalista
+        return 4;
+    }
+ 
+}
+ 
